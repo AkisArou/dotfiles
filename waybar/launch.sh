0bc118c7f559deaa86ce326be87fbf46915dd90c
AkisArou <akisarou90@gmail.com>
2023-10-16 08:53:58 +0300

    change style

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -16,9 +16,4 @@ killall waybar
 # ----------------------------------------------------- 
 # Loading the configuration based on the username
 # ----------------------------------------------------- 
-if [[ $USER = "raabe" ]]
-then
-    waybar -c ~/dotfiles/waybar/myconfig & -s ~/dotfiles/waybar/style.css  
-else
-    waybar &
-fi 
+waybar -c ~/dotfiles/waybar/myconfig & -s ~/dotfiles/waybar/style.css  
