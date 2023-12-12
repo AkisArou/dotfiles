a9059c072e854df5694ff64bb58ca28b268add40
AkisArou <akisarou90@gmail.com>
2023-12-12 14:20:48 +0200

    ssh

--- scripts/add-ssh-key-to-agent.sh
+++ scripts/add-ssh-key-to-agent.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Initialize variables with default values
 silent_mode=false
@@ -19,7 +19,8 @@ done
 # Check if SSH agent is running
 if [ -z "$SSH_AUTH_SOCK" ] || [ -z "$SSH_AGENT_PID" ]; then
 	! $silent_mode && echo "Starting SSH agent..."
-	eval "$(ssh-agent -s)" >/dev/null 2>&1
+	eval "$(ssh-agent -s)"
+	# >/dev/null 2>&1
 fi
 
 ssh_key_path="$HOME/.ssh/id_rsa"
@@ -27,7 +28,8 @@ ssh_key_path="$HOME/.ssh/id_rsa"
 # Check if SSH key exists
 if [ -f "$ssh_key_path" ]; then
 	! $silent_mode && echo "SSH key found. Adding to SSH agent."
-	ssh-add "$ssh_key_path" >/dev/null 2>&1
+	ssh-add "$ssh_key_path"
+	# >/dev/null 2>&1
 else
 	! $silent_mode && echo "No SSH key found at $ssh_key_path."
 fi
