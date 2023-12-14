b4ed4b31c481c51f3cf9aecb19af82479d6ad40c
AkisArou <akisarou90@gmail.com>
2023-12-14 20:08:10 +0200

    add css variables ls for nvim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -28,3 +28,60 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   end,
 })
 --
+
+-- CSS variables lsp
+
+local file_path = os.getenv("HOME")
+  .. "/.asdf/installs/nodejs/20.10.0/lib/node_modules/css-variables-language-server/dist/index.js"
+
+-- Patch lsp (null TypeError in line 249)
+local com = "grep -F "
+  .. '\'settings = { lookupFiles: ["**/*.less", "**/*.scss\''
+  .. " "
+  .. file_path
+  .. " || "
+  .. 'sed -i \'249i\\   settings = { lookupFiles: ["**/*.less", "**/*.scss", "**/*.sass", "**/*.css"], blacklistFolders: ["**/.cache", "**/.DS_Store", "**/.git", "**/.hg", "**/.next", "**/.svn", "**/bower_components", "**/CVS", "**/dist", "**/node_modules", "**/tests", "**/tmp"] }\' '
+  .. file_path
+
+vim.fn.system(com)
+
+local css_variables_lsp_cmd = {
+  "node",
+  file_path,
+  "--nolazy",
+  "--stdio",
+}
+
+local c = vim.lsp.start_client({
+  config = {
+    cmd = css_variables_lsp_cmd,
+  },
+  settings = {
+    lookupFiles = { "**/*.less", "**/*.scss", "**/*.sass", "**/*.css" },
+    blacklistFolders = {
+      "**/.cache",
+      "**/.DS_Store",
+      "**/.git",
+      "**/.hg",
+      "**/.next",
+      "**/.svn",
+      "**/bower_components",
+      "**/CVS",
+      "**/dist",
+      "**/node_modules",
+      "**/tests",
+      "**/tmp",
+    },
+  },
+  name = "css-variables-language-server",
+  cmd = css_variables_lsp_cmd,
+  root_dir = vim.loop.cwd(),
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.css", "*.scss" },
+  callback = function()
+    vim.lsp.buf_attach_client(0, c)
+  end,
+})
+--
