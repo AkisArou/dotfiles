7b7f2a7f49a3d6b7a0042e6cf9508db8ac23c4ad
AkisArou <akisarou90@gmail.com>
2023-12-15 16:55:16 +0200

    fix sway gaps

--- sway/config.d/gaps.sh
+++ sway/config.d/gaps.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-if (swaymsg --type get_outputs | grep 'DP-2'); then
+# Get the output of swaymsg --type get_outputs and filter for 'DP-2'
+output=$(swaymsg --type get_outputs | grep 'DP-2')
+
+# Check if the output is empty
+if [ -n "$output" ]; then
+	# If not empty, run the specified swaymsg commands
 	swaymsg "smart_gaps inverse_outer"
-	swaymsg "gaps horizontal 100"
+	swaymsg "gaps horizontal 1500"
 fi
