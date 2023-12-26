0b99dcbb3842e397c316c5b65d3d54c18b65645f
AkisArou <akisarou90@gmail.com>
2023-12-26 23:25:06 +0200

    change colorschema in nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,10 @@
 return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "habamax",
+    },
+  },
   { "ellisonleao/gruvbox.nvim" },
   { "rose-pine/neovim", name = "rose-pine" },
   { "EdenEast/nightfox.nvim" },
