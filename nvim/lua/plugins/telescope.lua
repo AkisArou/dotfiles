0a43c231da7e637ec718e4f6808e675bad819906
AkisArou <akisarou90@gmail.com>
2024-01-16 03:34:47 +0200

    change file pickers layout for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,16 @@
 local Util = require("lazyvim.util")
 
+local file_pickers_config = {
+  sorting_strategy = "ascending",
+  -- layout_strategy = "flex",
+  layout_config = {
+    -- height = 0.95,
+    prompt_position = 'top',
+    -- preview_height = 0,
+    preview_width = 0.2
+  }
+}
+
 return {
   {
     "telescope.nvim",
@@ -7,9 +18,15 @@ return {
       { "<leader>ff", Util.telescope("files", { cwd = false }), desc = "Find Files (root dir)" },
       { "<leader>fF", Util.telescope("files"),                  desc = "Find Files (cwd)" },
     },
+    opts = {
+      pickers = {
+        git_files = file_pickers_config,
+        find_files = file_pickers_config,
+      },
+    },
     extentions = {
       coc = {
-        theme = 'ivy',
+        theme = "ivy",
         prefer_locations = true, -- always use Telescope locations to preview definitions/declarations/implementations etc
       }
     },
