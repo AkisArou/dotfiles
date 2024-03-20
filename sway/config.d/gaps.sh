5988fb932b8f75f154e29a13b9c9b76cbf2e6451
AkisArou <akisarou90@gmail.com>
2024-03-20 18:02:37 +0200

    sway

--- sway/config.d/gaps.sh
+++ sway/config.d/gaps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the output of swaymsg --type get_outputs and filter for 'DP-2'
-output=$(swaymsg --type get_outputs | grep 'DP-2')
+output=$(swaymsg --type get_outputs | grep 'DP-4')
 
 # Check if the output is empty
 if [ -n "$output" ]; then
