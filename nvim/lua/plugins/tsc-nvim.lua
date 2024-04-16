90be5af5119a38afa9e09f391d8d192a38a46b63
AkisArou <akisarou90@gmail.com>
2024-04-16 15:51:17 +0300

    tsc nvim

--- nvim/lua/plugins/tsc-nvim.lua
+++ nvim/lua/plugins/tsc-nvim.lua
@@ -1,12 +1,14 @@
 return {
   "dmmulroy/tsc.nvim",
   event = "VimEnter",
-  enabled = false,
+  enabled = true,
   config = function()
     local cwd = vim.fn.getcwd()
     require("tsc").setup({
       use_trouble_qflist = false,
       auto_start_watch_mode = true,
+      auto_close_qflist = false,
+      auto_focus_qflist = false,
       -- bin_path = cwd .. " ./node-modules/.bin/tsc " .. cwd,
       raw = "--build " .. cwd .. "/tsconfig.json --watch",
       flags = {
