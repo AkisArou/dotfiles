2a8ee0f715372e7ec2bcadbe51daa0d658a6d00f
AkisArou <akisarou90@gmail.com>
2024-05-24 15:11:30 +0300

    autoclose noname buffers in nvim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -39,6 +39,42 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   end,
 })
 
+-- Function to close empty and unnamed buffers
+function Close_empty_unnamed_buffers()
+  -- Get a list of all buffers
+  local buffers = vim.api.nvim_list_bufs()
+
+  -- Iterate over each buffer
+  for _, bufnr in ipairs(buffers) do
+    -- Check if the buffer is empty and doesn't have a name
+    if
+      vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_name(bufnr) == ""
+      -- and vim.api.nvim_buf_get_option(bufnr, "buftype") == ""
+    then
+      -- -- Get all lines in the buffer
+      -- local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+      --
+      -- -- Initialize a variable to store the total number of characters
+      -- local total_characters = 0
+      --
+      -- -- Iterate over each line and calculate the number of characters
+      -- for _, line in ipairs(lines) do
+      --   total_characters = total_characters + #line
+      -- end
+
+      -- Close the buffer if it's empty:
+      -- if total_characters == 0 then
+      vim.api.nvim_buf_delete(bufnr, {
+        force = true,
+      })
+      -- end
+    end
+  end
+end
+
+-- Clear the mandatory, empty, unnamed buffer when a real file is opened:
+vim.api.nvim_command("autocmd BufReadPost * lua Close_empty_unnamed_buffers()")
+
 -- CSS variables lsp
 
 -- local file_path = util.get_css_variables_language_server_path()
