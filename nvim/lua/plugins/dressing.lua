9ea96c7ec5876ba547835d5e24f2431b3402b94c
AkisArou <akisarou90@gmail.com>
2024-05-28 12:14:48 +0300

    lsp-signature

--- nvim/lua/plugins/dressing.lua
+++ nvim/lua/plugins/dressing.lua
@@ -1,4 +1,13 @@
 return {
   "stevearc/dressing.nvim",
   event = "VeryLazy",
+  opts = {
+    input = {
+      -- When true, <Esc> will close the modal
+      insert_only = false,
+
+      -- When true, input will start in insert mode.
+      start_in_insert = true,
+    },
+  },
 }
---
+++ nvim/lua/plugins/lsp/lsp-signature.lua
@@ -0,0 +1,10 @@
+return {
+  "ray-x/lsp_signature.nvim",
+  event = "VeryLazy",
+  opts = {
+    floating_window = false, -- show hint in a floating window, set to false for virtual text only mode
+  },
+  config = function(_, opts)
+    require("lsp_signature").setup(opts)
+  end,
+}
