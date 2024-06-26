e0dbb56bfaa06e1db53141a0c6cadf928e5c5807
AkisArou <akisarou90@gmail.com>
2024-06-26 11:58:19 +0300

    polybar init

--- polybar/init.sh
+++ polybar/init.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/zsh
 
+FONT_FAMILY="MesloLGS Nerd Font Mono"
+
 if [[ "$HOST" == "akisarou-desktop" ]]; then
-	export POLY_FONT="MesloLGS Nerd Font Mono:size=10"
+	export POLY_FONT="$FONT_FAMILY:size=10"
 	export POLY_BAR_HEIGHT="20pt"
 else
-	export POLY_FONT="MesloLGS Nerd Font Mono:size=20"
+	export POLY_FONT="$FONT_FAMILY:size=20"
 	export POLY_BAR_HEIGHT="30pt"
 fi
 
