d5dec7d55cdf829367ffa6863d19b0bda36757a8
AkisArou <akisarou90@gmail.com>
2024-07-09 02:53:44 +0300

    telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -15,17 +15,13 @@ return {
   },
   config = function()
     local telescope = require("telescope")
-    local actions = require("telescope.actions")
 
     telescope.setup({
       pickers = {
         find_files = {
           sorting_strategy = "ascending",
-          -- layout_strategy = "flex",
           layout_config = {
-            -- height = 0.95,
             prompt_position = "top",
-            -- preview_height = 0,
             preview_width = 0.25,
           },
         },
@@ -33,11 +29,8 @@ return {
 
       defaults = {
         path_display = { "truncate " },
-        mappings = {
-          i = {
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
+        prompt_prefix = " ",
+        selection_caret = " ",
       },
     })
 
@@ -52,49 +45,5 @@ return {
 
     keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics bufnr=0<cr>", { desc = "Document diagnostics" })
     keymap.set("n", "<leader>sD", "<cmd>Telescope diagnostics<cr>", { desc = "Workspace diagnostics" })
-  end,
-
-  opts = function()
-    local actions = require("telescope.actions")
-
-    local open_with_trouble = function(...)
-      return require("trouble.providers.telescope").open_with_trouble(...)
-    end
-    local open_selected_with_trouble = function(...)
-      return require("trouble.providers.telescope").open_selected_with_trouble(...)
-    end
-
-    return {
-      defaults = {
-        prompt_prefix = " ",
-        selection_caret = " ",
-        -- open files in the first window that is an actual file.
-        -- use the current window if no other window is available.
-        get_selection_window = function()
-          local wins = vim.api.nvim_list_wins()
-          table.insert(wins, 1, vim.api.nvim_get_current_win())
-          for _, win in ipairs(wins) do
-            local buf = vim.api.nvim_win_get_buf(win)
-            if vim.bo[buf].buftype == "" then
-              return win
-            end
-          end
-          return 0
-        end,
-        mappings = {
-          i = {
-            ["<c-t>"] = open_with_trouble,
-            ["<a-t>"] = open_selected_with_trouble,
-            ["<C-Down>"] = actions.cycle_history_next,
-            ["<C-Up>"] = actions.cycle_history_prev,
-            ["<C-f>"] = actions.preview_scrolling_down,
-            ["<C-b>"] = actions.preview_scrolling_up,
-          },
-          n = {
-            ["q"] = actions.close,
-          },
-        },
-      },
-    }
   end,
 }
