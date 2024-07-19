d9057c7db43d2f7bcb5c8f5f0672a8590c5f78a0
AkisArou <akisarou90@gmail.com>
2024-07-19 03:30:49 +0300

    config

--- 4-config.sh
+++ 4-config.sh
@@ -54,10 +54,6 @@ source ~/.zshrc
 
 ## Add asdf plugins
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin add java https://github.com/halcyon/asdf-java.git
-asdf plugin-add maven
-asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
-asdf plugin add kotlin https://github.com/asdf-community/asdf-kotlin.git
 
 
 # Read .tool-versions file and install the specified versions
