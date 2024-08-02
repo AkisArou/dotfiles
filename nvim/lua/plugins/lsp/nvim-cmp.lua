fe43f922a96c0b734345b0012de40b61c9f1ca46
AkisArou <akisarou90@gmail.com>
2024-08-02 22:33:29 +0300

    temporary disable nvim cmp buffer

--- nvim/lua/plugins/lsp/nvim-cmp.lua
+++ nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -54,20 +54,20 @@ return {
       },
       -- Group 2
       {
-        {
-          name = "buffer",
-          option = {
-            get_bufnrs = function()
-              local buf = vim.api.nvim_get_current_buf()
-              local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
-              if byte_size > 1024 * 1024 then -- 1 Megabyte max
-                return {}
-              end
-              return { buf }
-            end,
-          },
-        },
-        { name = "luasnip" },
+        -- {
+        --   name = "buffer",
+        --   option = {
+        --     get_bufnrs = function()
+        --       local buf = vim.api.nvim_get_current_buf()
+        --       local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+        --       if byte_size > 1024 * 1024 then -- 1 Megabyte max
+        --         return {}
+        --       end
+        --       return { buf }
+        --     end,
+        --   },
+        -- },
+        -- { name = "luasnip" },
       }
     )
 
