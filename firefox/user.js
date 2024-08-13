76205cd69eccdc0fe4e6ad9edecb9c87a2395ceb
AkisArou <akisarou90@gmail.com>
2024-08-13 23:40:11 +0300

    firefox config

--- firefox/user.js
+++ firefox/user.js
@@ -1,3 +1,4 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.download.always_ask_before_handling_new_types", true);
 user_pref("browser.quitShortcut.disabled", true);
 user_pref("browser.fullscreen.autohide", false);
@@ -12,3 +13,4 @@ user_pref("media.autoplay.default", 0); // 0 = Allow all, 1 = Block non-muted me
 user_pref("media.autoplay.allow-muted", true); // Allow muted autoplay
 user_pref("media.autoplay.blocking_policy", 0); // Disable any blocking policy for autoplay
 user_pref("browser.startup.page", 3); // 3 = Restore previous session
+user_pref("layout.css.always_underline_links", false);
