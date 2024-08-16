3f4ac8bbdf2e53f37397799411f9008e4046f9c7
AkisArou <akisarou90@gmail.com>
2024-08-16 21:16:40 +0300

    firefox

--- firefox/user.js
+++ firefox/user.js
@@ -14,3 +14,27 @@ user_pref("media.autoplay.allow-muted", true); // Allow muted autoplay
 user_pref("media.autoplay.blocking_policy", 0); // Disable any blocking policy for autoplay
 user_pref("browser.startup.page", 3); // 3 = Restore previous session
 user_pref("layout.css.always_underline_links", false);
+
+/*
+┌───────────────────────────────────────────────┐
+│░█▀▀░█▄█░█▀█░█▀█░▀█▀░█░█░█▀▀░█▀█░█░█░░░░▀▀█░█▀▀│
+│░▀▀█░█░█░█░█░█░█░░█░░█▀█░█▀▀░█░█░▄▀▄░░░░░░█░▀▀█│
+│░▀▀▀░▀░▀░▀▀▀░▀▀▀░░▀░░▀░▀░▀░░░▀▀▀░▀░▀░▀░░▀▀░░▀▀▀│
+└───────────────────────────────────────────────┘
+*/
+user_pref("general.smoothScroll", true);
+user_pref("mousewheel.default.delta_multiplier_y", 275);
+user_pref("general.smoothScroll", true);
+user_pref("mousewheel.default.delta_multiplier_y", 300);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll", true);
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 650);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 25);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", 2.0);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
+user_pref("general.smoothScroll.currentVelocityWeighting", 1.0);
+user_pref("general.smoothScroll.stopDecelerationWeighting", 1.0);
+user_pref("mousewheel.default.delta_multiplier_y", 300);
