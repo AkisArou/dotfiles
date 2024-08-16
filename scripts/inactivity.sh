123474e2dafd5adb10aafe2aef2a49cce866786f
AkisArou <akisarou90@gmail.com>
2024-08-16 20:46:57 +0300

    inactivity

--- scripts/inactivity.sh
+++ scripts/inactivity.sh
@@ -2,18 +2,10 @@
 
 export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 
-if [[ "$HOST" == "arch-xps" ]]; then
-  export SUSPEND_TIME=1800
-else
-  export SUSPEND_TIME=$((3600 * 3))
-fi
-
 xidlehook \
   --not-when-fullscreen \
   --not-when-audio \
   --timer 120 \
   'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
   'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
-  --timer $SUSPEND_TIME \
-  'systemctl suspend' \
   ''
