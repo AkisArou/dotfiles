5f3be6b337afa053c72e843b505e24a3ad9b84ba
AkisArou <akisarou90@gmail.com>
2024-08-23 23:35:28 +0300

    auto-session

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -5,17 +5,16 @@ vim.cmd([[
   augroup END
 ]])
 
-function Close_empty_unnamed_buffers()
-  local buffers = vim.api.nvim_list_bufs()
-
-  for _, bufnr in ipairs(buffers) do
-    if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_name(bufnr) == "" then
-      vim.api.nvim_buf_delete(bufnr, {
-        force = true,
-      })
-    end
-  end
-end
-
--- Register the autocommand after Vim has entered
-vim.api.nvim_command("autocmd VimEnter * autocmd BufReadPost * lua Close_empty_unnamed_buffers()")
+-- function Close_empty_unnamed_buffers()
+--   local buffers = vim.api.nvim_list_bufs()
+--
+--   for _, bufnr in ipairs(buffers) do
+--     if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_name(bufnr) == "" then
+--       vim.api.nvim_buf_delete(bufnr, {
+--         force = true,
+--       })
+--     end
+--   end
+-- end
+--
+-- vim.api.nvim_command("autocmd BufReadPost * lua Close_empty_unnamed_buffers()")
--- nvim/lua/plugins/auto-session.lua
+++ nvim/lua/plugins/auto-session.lua
@@ -1,3 +1,15 @@
+function Close_empty_unnamed_buffers()
+  local buffers = vim.api.nvim_list_bufs()
+
+  for _, bufnr in ipairs(buffers) do
+    if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_name(bufnr) == "" then
+      vim.api.nvim_buf_delete(bufnr, {
+        force = true,
+      })
+    end
+  end
+end
+
 return {
   "rmagatti/auto-session",
   lazy = false,
@@ -5,8 +17,14 @@ return {
   config = function()
     vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
+    -- Register the autocommand after Vim has entered
+
     require("auto-session").setup({
       auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+      post_restore_cmds = {
+        -- Initiated here because has problems with auto-session
+        ':lua vim.api.nvim_command("autocmd BufReadPost * lua Close_empty_unnamed_buffers()")',
+      },
       -- log_level = "debug",
     })
   end,
