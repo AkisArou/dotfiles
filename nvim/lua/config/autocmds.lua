d33942270a1573ed8c1dfebb025406508aa4edc5
AkisArou <akisarou90@gmail.com>
2024-08-24 02:45:56 +0300

    auto-session

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -5,16 +5,20 @@ vim.cmd([[
   augroup END
 ]])
 
--- function Close_empty_unnamed_buffers()
---   local buffers = vim.api.nvim_list_bufs()
---
---   for _, bufnr in ipairs(buffers) do
---     if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_name(bufnr) == "" then
---       vim.api.nvim_buf_delete(bufnr, {
---         force = true,
---       })
---     end
---   end
--- end
---
--- vim.api.nvim_command("autocmd BufReadPost * lua Close_empty_unnamed_buffers()")
+function Close_empty_unnamed_buffers()
+  local buffers = vim.api.nvim_list_bufs()
+
+  for _, bufnr in ipairs(buffers) do
+    if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_name(bufnr) == "" then
+      vim.api.nvim_buf_delete(bufnr, {
+        force = true,
+      })
+    end
+  end
+end
+
+local function register_autocmd()
+  vim.api.nvim_command("autocmd BufReadPost * lua Close_empty_unnamed_buffers()")
+end
+
+vim.defer_fn(register_autocmd, 1000)
--- nvim/lua/plugins/auto-session.lua
+++ nvim/lua/plugins/auto-session.lua
@@ -1,15 +1,3 @@
-function Close_empty_unnamed_buffers()
-  local buffers = vim.api.nvim_list_bufs()
-
-  for _, bufnr in ipairs(buffers) do
-    if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_name(bufnr) == "" then
-      vim.api.nvim_buf_delete(bufnr, {
-        force = true,
-      })
-    end
-  end
-end
-
 return {
   "rmagatti/auto-session",
   lazy = false,
@@ -18,10 +6,6 @@ return {
 
     require("auto-session").setup({
       auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
-      post_restore_cmds = {
-        -- Initiated here because has problems with auto-session
-        ':lua vim.api.nvim_command("autocmd BufReadPost * lua Close_empty_unnamed_buffers()")',
-      },
       -- log_level = "debug",
     })
   end,
