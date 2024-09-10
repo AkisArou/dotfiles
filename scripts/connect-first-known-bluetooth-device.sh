40741db83abd3c167db8dee7beb1f411c7a0c61c
AkisArou <akisarou90@gmail.com>
2024-09-11 01:06:54 +0300

    connect-first-known-bluetooth-device

--- scripts/connect-first-known-bluetooth-device.sh
+++ scripts/connect-first-known-bluetooth-device.sh
@@ -1,20 +1,26 @@
 #!/bin/zsh
 
-dev=$(bluetoothctl devices)
-mac_address=$(echo "$dev" | grep -oE '([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}')
-echo "$mac_address"
+# Get list of devices (MAC address and device name)
+devices=$(bluetoothctl devices)
+# Extract both MAC address and the device name
+device_info=$(echo "$devices" | grep -Eo '([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2} .+')
+mac_address=$(echo "$device_info" | awk '{print $1}')
+device_name=$(echo "$device_info" | awk '{for(i=2;i<=NF;++i) printf $i " "; print ""}')
+
+echo "MAC Address: $mac_address"
+echo "Device Name: $device_name"
 
 # Check if there are connected devices
-connected_devices=$(bluetoothctl info | grep "Connected" | awk '{print $2}')
+connected_status=$(bluetoothctl info "$mac_address" | grep "Connected" | awk '{print $2}')
 
-text=""
+echo "Connected status: $connected_status"
 
-if [ "$connected_devices" == "yes" ]; then
+if [ "$connected_status" = "yes" ]; then
   bluetoothctl disconnect "$mac_address"
-  text="Disconnected from $mac_address"
+  text="Disconnected $device_name ($mac_address)"
 else
   bluetoothctl connect "$mac_address"
-  text="Connected to $mac_address"
+  text="Connected $device_name ($mac_address)"
 fi
 
 echo "$text"
