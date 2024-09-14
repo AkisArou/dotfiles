e14bb18250c636cadd47b2a0ebf129fd3d848046
AkisArou <akisarou90@gmail.com>
2024-09-14 21:05:37 +0300

    fzf keybinds

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -34,6 +34,7 @@ export QT_SCALE_FACTOR=2
 export QT_FONT_DPI=243
 export FZF_DEFAULT_COMMAND="fd --type file --follow --hidden --exclude .git"
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --bind ctrl-d:page-down,ctrl-u:page-up
   --highlight-line \
   --info=inline-right \
   --ansi \
