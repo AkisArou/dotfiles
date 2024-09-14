e1d482d36e391f1e19240f67ceaca3456da88dfb
AkisArou <akisarou90@gmail.com>
2024-09-14 03:40:39 +0300

    zshrc

--- zsh/pnpm-completion.sh
+++ zsh/pnpm-completion.sh
@@ -1,3 +1,5 @@
+source /usr/share/zsh/plugins/pnpm-shell-completion/pnpm-shell-completion.zsh
+
 _fzf_complete_pnpm() {
   _fzf_complete --multi --reverse --prompt="pnpm run> " -- "$@" < <(
     cat package.json | jq -r '.scripts | keys[]'
