281d0482e3ea6144abfed0ef289fc3ca239e9d31
AkisArou <akisarou90@gmail.com>
2024-09-30 16:19:10 +0300

    neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -59,8 +59,8 @@ return {
           expander_highlight = "NeoTreeExpander",
         },
         icon = {
-          folder_closed = "",
-          folder_open = "",
+          folder_closed = "+",
+          folder_open = "-",
           folder_empty = "󰜌",
           -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
           -- then these will never be used.
