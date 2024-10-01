e96e0cb9e49f2607c968a0531aa56f29be273546
AkisArou <akisarou90@gmail.com>
2024-10-01 13:58:33 +0300

    nvim

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  enabled = false,
+  enabled = true,
   config = function()
     -- Eviline config for lualine
     -- Author: shadmansaleh
