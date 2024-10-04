61f3dc781aace7610fb55398652081ad6153b9b6
AkisArou <akisarou90@gmail.com>
2024-10-04 03:16:45 +0300

    colorscheme nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,6 @@ local themes = {
   tokyonight = {
     name = "tokyonight",
     repo = "folke/tokyonight.nvim",
-    commit = "e52c41314e83232840d6970e6b072f9fba242eb9",
     config = function()
       vim.opt.background = "dark"
     end,
@@ -14,27 +13,20 @@ local themes = {
     config = function()
       vim.o.background = "dark"
       require("vscode").setup()
-      require("vscode").load()
     end,
   },
 }
 
 local selectedTheme = themes.vscode
 
-if vim.fn.has("termguicolors") == 1 then
-  vim.o.termguicolors = true
-end
-
 local M = {
   selectedTheme.repo,
-  commit = selectedTheme.commit,
+  name = selectedTheme.name,
   event = "VimEnter",
   opts = selectedTheme.opts,
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  lazy = false,
+  priority = 1000,
 }
-
-M.name = selectedTheme.name
 
 function M.config()
   if selectedTheme.config ~= nil then
