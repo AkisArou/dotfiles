5cc2a5f29560bc44787d6c27324a0504e669dc9b
AkisArou <akisarou90@gmail.com>
2024-10-07 02:41:18 +0300

    rainbow-delimiters

--- nvim/lua/plugins/rainbow-delimiters.lua
+++ nvim/lua/plugins/rainbow-delimiters.lua
@@ -1,3 +1,11 @@
 return {
   "hiphish/rainbow-delimiters.nvim",
+  config = function()
+    vim.g.rainbow_delimiters = {
+      query = {
+        javascript = "rainbow-parens",
+        tsx = "rainbow-parens",
+      },
+    }
+  end,
 }
