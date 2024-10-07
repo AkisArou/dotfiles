0339a43ef9ba8286c09d3bca816496cb7a96e926
AkisArou <akisarou90@gmail.com>
2024-10-07 19:06:37 +0300

    toggleterm

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -5,20 +5,32 @@ return {
   opts = function()
     local Terminal = require("toggleterm.terminal").Terminal
 
-    local tod0 = Terminal:new({ cmd = "tod0", hidden = true, direction = "float" })
+    local plain = Terminal:new({
+      hidden = true,
+      direction = "float",
 
-    function Tod0Toggle()
-      tod0:toggle()
-    end
-
-    local plain = Terminal:new({ hidden = true, direction = "float" })
+      on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<esc>", "<cmd>close<CR>", { noremap = true, silent = true })
+      end,
+      on_close = function()
+        vim.cmd("startinsert!")
+      end,
+    })
 
     function TogglePlainTerm()
       plain:toggle()
     end
 
-    vim.api.nvim_set_keymap("n", "<leader>td", "<cmd>lua Tod0Toggle()<CR>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>lua TogglePlainTerm()<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("t", "<Esc><Esc>", "<cmd>lua TogglePlainTerm()<CR>", { desc = "Exit terminal mode" })
+
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+      vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+    end
+
+    -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+    vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
   end,
 }
