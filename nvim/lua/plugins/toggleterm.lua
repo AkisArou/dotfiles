336fe123ea92760bdc839172fb6402fe7b2bdc5a
AkisArou <akisarou90@gmail.com>
2024-10-07 19:09:38 +0300

    toggleterm

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -8,7 +8,6 @@ return {
     local plain = Terminal:new({
       hidden = true,
       direction = "float",
-
       on_open = function(term)
         vim.cmd("startinsert!")
         vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<esc>", "<cmd>close<CR>", { noremap = true, silent = true })
@@ -27,7 +26,6 @@ return {
     function _G.set_terminal_keymaps()
       local opts = { buffer = 0 }
       vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-      vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
     end
 
     -- if you only want these mappings for toggle term use term://*toggleterm#* instead
