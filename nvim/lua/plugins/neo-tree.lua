6d40b1df73ce08db51240df57c4e6053568dfb64
AkisArou <akisarou90@gmail.com>
2024-10-09 16:37:29 +0300

    neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -170,7 +170,6 @@ return {
           --}
           ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
           ["q"] = "close_window",
-          ["<C-c>"] = "close_window",
           ["R"] = "refresh",
           ["?"] = "show_help",
           ["<"] = "prev_source",
