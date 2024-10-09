528c1964cb9e83f4bb34a46a6d775540fa4fdb86
AkisArou <akisarou90@gmail.com>
2024-10-09 04:54:37 +0300

    lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -147,9 +147,10 @@ return {
 
     ins_right({
       function()
-        if TSC_ERRORS_COUNT == 0 then
+        if TSC_ERRORS_COUNT == 0 or TSC_ERRORS_COUNT == nil then
           return ""
         end
+
         return "Project errors: " .. tostring(TSC_ERRORS_COUNT)
       end,
       color = { fg = colors.red, gui = "bold" },
