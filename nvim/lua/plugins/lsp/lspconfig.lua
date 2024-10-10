fea953aca5d4154c11d28975801a5f6ef2cfaf96
AkisArou <akisarou90@gmail.com>
2024-10-10 20:20:02 +0300

    lspconfig file operations

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -18,6 +18,17 @@ return {
 
     local lspconfig = require("lspconfig")
 
+    -- Set global defaults for all servers
+    lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
+      capabilities = vim.tbl_deep_extend(
+        "force",
+        vim.lsp.protocol.make_client_capabilities(),
+        -- returns configured operations if setup() was already called
+        -- or default operations if not
+        require("lsp-file-operations").default_capabilities()
+      ),
+    })
+
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
     local keymap = vim.keymap
