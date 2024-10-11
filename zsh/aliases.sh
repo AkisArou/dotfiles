00d86f12b053b0d9425e10e0750c9daff7b81201
AkisArou <akisarou90@gmail.com>
2024-10-12 02:07:11 +0300

    zsh aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,21 +1,21 @@
 alias sudo="sudo -E"
-alias ta='tmux a'
-alias v='nvim --listen /tmp/nvim.pipe'
-alias r='yazi'
-alias dir='dir --color=auto'
-alias grep='grep --color=auto'
-alias ip='ip -color=auto'
-alias copy='xclip -sel clip'
-alias grep='grep --color=auto'
+alias ta="tmux a"
+alias v="nvim --listen /tmp/nvim.pipe"
+alias r="yazi"
+alias dir="dir --color=auto"
+alias grep="grep --color=auto"
+alias ip="ip -color=auto"
+alias copy="xclip -sel clip"
+alias grep="grep --color=auto"
 
 # confirm before overwriting something
 alias cp="cp -i"
-alias mv='mv -i'
-alias rm='rm -i'
+alias mv="mv -i"
+alias rm="rm -i"
 
 # easier to read disk
-alias df='df -h'     # human-readable sizes
-alias free='free -m' # show sizes in MB
+alias df="df -h"     # human-readable sizes
+alias free="free -m" # show sizes in MB
 
 if command -v bat &>/dev/null; then
   alias bat="bat -pp --theme \"Visual Studio Dark+\""
