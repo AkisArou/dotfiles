17d97315ba08c9f39a88bf4fed5d52bb7de851fd
AkisArou <akisarou90@gmail.com>
2024-10-13 01:30:22 +0300

    install-sway

--- 2-install-sway.sh
+++ 2-install-sway.sh
@@ -1,5 +1,18 @@
-sway
-wl-clipboard
-polkit
-waybar
-tofi
+#!/bin/bash
+
+source "$(dirname "$0")/scripts/library.sh"
+
+packages=(
+  "sway"
+  "polkit"
+  "wl-clipboard"
+  "waybar"
+  "tofi"
+)
+
+# ------------------------------------------------------
+# Install required packages
+# ------------------------------------------------------
+install_packages "${packages[@]}"
+
+echo "DONE! Please reboot your system!"
