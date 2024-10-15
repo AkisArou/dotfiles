c5127b3d9d4a6f205f1d3e593f345a8465c0fafa
AkisArou <akisarou90@gmail.com>
2024-10-15 20:40:02 +0300

    yazi

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -4,22 +4,23 @@ return {
   keys = {
     {
       "<leader>e",
-      "<cmd>Yazi toggle<cr>",
+      "<cmd>Yazi<cr>",
       desc = "Resume the last yazi session",
     },
-    {
-      "<leader>cw",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
-    },
+    -- {
+    --   "<leader>cw",
+    --   "<cmd>Yazi cwd<cr>",
+    --   desc = "Open the file manager in nvim's working directory",
+    -- },
   },
-  ---@type YaziConfig
-  opts = {
-    open_for_directories = true,
-    yazi_floating_window_border = "shadow",
-    floating_window_scaling_factor = 1,
-    keymaps = {
-      show_help = "<f1>",
-    },
-  },
+  config = function()
+    require("yazi").setup({
+      open_for_directories = true,
+      yazi_floating_window_border = "shadow",
+      floating_window_scaling_factor = 1,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    })
+  end,
 }
