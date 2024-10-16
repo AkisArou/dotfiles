ff67fa7090efbc37ca0d63dd4b60ad059111ac2f
AkisArou <akisarou90@gmail.com>
2024-10-16 04:43:19 +0300

    zsh functions

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -23,20 +23,6 @@ sus_desktop() {
   ssh "$DESKTOP" 'sudo -S systemctl suspend'
 }
 
-ccd() {
-  local dir
-  dir="$(
-    find "${1:-.}" -path '*/\.*' -prune -o -type d -print 2>/dev/null |
-      fzf +m \
-        --preview='tree -C {} | head -n $FZF_PREVIEW_LINES' \
-        --preview-window='right:hidden:wrap' \
-        --bind=ctrl-v:toggle-preview \
-        --bind=ctrl-x:toggle-sort \
-        --header='(view:ctrl-v) (sort:ctrl-x)'
-  )" || return
-  cd "$dir" || return
-}
-
 fv() {
   local file
   file="$(
