d540e28b94aeccc8aa82b76124b18c91b3b05067
AkisArou <akisarou90@gmail.com>
2024-10-25 13:00:26 +0300

    environment

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -26,13 +26,6 @@ export XDG_RUNTIME_DIR=/run/user/$(id -u)
 export XKB_DEFAULT_LAYOUT="us,gr"
 export XKB_DEFAULT_OPTIONS="grp:alt_shift_toggle"
 
-# wayland specific
-export MOZ_ENABLE_WAYLAND=1
-export MOZ_WAYLAND_USE_VAAPI=1
-export GDK_BACKEND=wayland
-#export QT_QPA_PLATFORM="wayland;xcb"
-export QT_QPA_PLATFORM=wayland
-
 export TERMINAL="foot"
 export TERM="xterm-256color"
 export EDITOR="nvim"
