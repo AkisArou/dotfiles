f6319dae16473e40c17ff6887c09c1b766665b0e
AkisArou <akisarou90@gmail.com>
2024-10-29 18:40:19 +0200

    nodejs ver

--- 4-config.sh
+++ 4-config.sh
@@ -33,6 +33,7 @@ asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add java https://github.com/halcyon/asdf-java.git
 asdf plugin add kotlin https://github.com/asdf-community/asdf-kotlin.git
 asdf plugin add maven
+asdf plugin add helm
 
 # Read .tool-versions file and install the specified versions
 cd ~
