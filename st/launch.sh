83a75d5f0f8e8e5ec757ed71a47d49d903ef0386
AkisArou <akisarou90@gmail.com>
2024-11-11 15:27:20 +0200

    st

--- st/launch.sh
+++ st/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
 if [[ "$HOST" == "akisarou-desktop" ]]; then
-  ~/dotfiles/st/st -s 0.9 -f "MesloLGS Nerd Font Mono:pixelsize=12:antialias=true:autohint=true"
+  ~/dotfiles/st/st -s 0.9 -f "DM Mono:pixelsize=12:antialias=true:autohint=true"
 else
-  ~/dotfiles/st/st -s 0.9 -f "MesloLGS Nerd Font Mono:pixelsize=25:antialias=true:autohint=true"
+  ~/dotfiles/st/st -s 0.9 -f "DM Mono:pixelsize=25:antialias=true:autohint=true"
 fi
