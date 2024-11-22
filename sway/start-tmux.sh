4852997100f7a76de0e0aa6a3c2957ee15d8a796
AkisArou <akisarou90@gmail.com>
2024-11-22 03:35:19 +0200

    rm systemd tmux service

--- sway/start-tmux.sh
+++ sway/start-tmux.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/zsh
 
-# Source .zshrc to load environment variables
 source ~/dotfiles/zsh/exports.sh
 
-# Automatically start or attach to a tmux session on login if not already in one
-if command -v tmux &>/dev/null && [ -z "$TMUX" ]; then
-  tmux new-session -s default &
-fi
+tmux new-session -s default -d &
+tmux new-session -s work -d -c ~/nable-solutions &
