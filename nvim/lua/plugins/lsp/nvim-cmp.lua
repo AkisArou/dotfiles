633d83dab2e1ac66c224e13804b389e2eb331d8e
AkisArou <akisarou90@gmail.com>
2024-11-26 04:55:24 +0200

    nvim-cmp

--- nvim/lua/plugins/lsp/nvim-cmp.lua
+++ nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -1,13 +1,14 @@
 return {
-  -- "hrsh7th/nvim-cmp",
   "iguanacucumber/magazine.nvim",
+  name = "nvim-cmp",
   dependencies = {
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "saadparwaiz1/cmp_luasnip",
+    { "iguanacucumber/mag-nvim-lsp", name = "cmp-nvim-lsp", opts = {} },
+    { "iguanacucumber/mag-nvim-lua", name = "cmp-nvim-lua" },
+    { "iguanacucumber/mag-buffer", name = "cmp-buffer" },
+    { "iguanacucumber/mag-cmdline", name = "cmp-cmdline" },
+    { "https://codeberg.org/FelipeLema/cmp-async-path" },
   },
-  event = { "InsertEnter", "CmdlineEnter" },
+  event = { "BufEnter" },
   config = function()
     vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
 
@@ -41,57 +42,6 @@ return {
       TypeParameter = "  ",
     }
 
-    local default_cmp_sources = cmp.config.sources({
-      { name = "nvim_lsp", group_index = 1 },
-      {
-        name = "path",
-        option = {
-          trailing_slash = true,
-        },
-        group_index = 1,
-      },
-      {
-        name = "buffer",
-        Keyword_length = 10,
-        option = {
-          get_bufnrs = function()
-            local buf = vim.api.nvim_get_current_buf()
-            local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
-            if byte_size > 1024 * 1024 then -- 1 Megabyte max
-              return {}
-            end
-            return { buf }
-          end,
-        },
-        group_index = 2,
-      },
-      { name = "luasnip", group_index = 2 },
-    })
-
-    vim.api.nvim_create_augroup("buffercmp", { clear = true })
-
-    vim.api.nvim_create_autocmd("BufEnter", {
-      callback = function()
-        if vim.bo.filetype == "typescriptreact" or vim.bo.filetype == "typescript" then
-          local sources = default_cmp_sources
-          for i = #sources, 1, -1 do
-            if sources[i].name == "buffer" then
-              table.remove(sources, i)
-            end
-          end
-
-          cmp.setup.buffer({
-            sources = sources,
-          })
-        else
-          cmp.setup.buffer({
-            sources = default_cmp_sources,
-          })
-        end
-      end,
-      group = "buffercmp",
-    })
-
     cmp.setup({
       preselect = cmp.PreselectMode.Item,
       completion = {
@@ -109,6 +59,33 @@ return {
         expand = function(args)
           require("luasnip").lsp_expand(args.body)
         end,
+      },
+      sources = {
+        { name = "nvim_lsp", group_index = 1 },
+        { name = "nvim_lua", group_index = 1 },
+        {
+          name = "async_path",
+          option = {
+            trailing_slash = true,
+          },
+          group_index = 1,
+        },
+        {
+          name = "buffer",
+          Keyword_length = 10,
+          option = {
+            get_bufnrs = function()
+              local buf = vim.api.nvim_get_current_buf()
+              local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+              if byte_size > 1024 * 1024 then -- 1 Megabyte max
+                return {}
+              end
+              return { buf }
+            end,
+          },
+          group_index = 2,
+        },
+        { name = "luasnip", group_index = 2 },
       },
       mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -167,7 +144,7 @@ return {
     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline(":", {
       sources = cmp.config.sources({
-        { name = "path" },
+        { name = "async_path" },
       }, {
         { name = "cmdline" },
       }),
