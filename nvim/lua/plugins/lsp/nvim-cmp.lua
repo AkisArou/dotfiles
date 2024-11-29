0d3d32d6eddbb64015678b59c29133071191c41b
AkisArou <akisarou90@gmail.com>
2024-11-29 18:27:16 +0200

    nvim-cmp

--- nvim/lua/plugins/lsp/nvim-cmp.lua
+++ nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -73,16 +73,16 @@ return {
         {
           name = "buffer",
           Keyword_length = 10,
-          option = {
-            get_bufnrs = function()
-              local buf = vim.api.nvim_get_current_buf()
-              local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
-              if byte_size > 1024 * 1024 then -- 1 Megabyte max
-                return {}
-              end
-              return { buf }
-            end,
-          },
+          -- option = {
+          --   get_bufnrs = function()
+          --     local buf = vim.api.nvim_get_current_buf()
+          --     local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+          --     if byte_size > 1024 * 1024 then -- 1 Megabyte max
+          --       return {}
+          --     end
+          --     return { buf }
+          --   end,
+          -- },
           group_index = 2,
         },
         { name = "luasnip", group_index = 2 },
