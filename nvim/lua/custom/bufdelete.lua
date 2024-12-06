67e9825441fcb338e9aea7ce49b42bfbd2410fc7
AkisArou <akisarou90@gmail.com>
2024-12-06 23:36:57 +0200

    bufdelete

--- nvim/lua/custom/bufdelete.lua
+++ nvim/lua/custom/bufdelete.lua
@@ -3,16 +3,16 @@ local M = {}
 function CloseBuffers(get_should_close)
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     if get_should_close(buf) then
-      local buf_name = vim.api.nvim_buf_get_name(buf)
-
       if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) then
+        local buf_name = vim.api.nvim_buf_get_name(buf)
+
         if not buf_name:match("NeogitStatus") then
           if vim.api.nvim_get_option_value("modified", { buf = buf }) then
             vim.api.nvim_command("bufdo silent! w")
           end
-
-          vim.api.nvim_buf_delete(buf, { force = true })
         end
+
+        vim.api.nvim_buf_delete(buf, { force = true })
       end
     end
   end
