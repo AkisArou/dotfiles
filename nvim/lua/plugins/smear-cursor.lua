0a327b39e7e2e5a09f70c39653d78ac599332160
AkisArou <akisarou90@gmail.com>
2024-12-08 23:28:00 +0200

    smear

--- nvim/lua/plugins/smear-cursor.lua
+++ nvim/lua/plugins/smear-cursor.lua
@@ -1,4 +1,10 @@
 return {
   "sphamba/smear-cursor.nvim",
-  opts = {},
+  opts = {
+    smear_between_buffers = true,
+    stiffness = 0.8, -- 0.6      [0, 1]
+    trailing_stiffness = 0.5, -- 0.3      [0, 1]
+    distance_stop_animating = 0.5, -- 0.1      > 0
+    hide_target_hack = false, -- true     boolean
+  },
 }
