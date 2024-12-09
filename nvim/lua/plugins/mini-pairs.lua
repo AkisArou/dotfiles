68b16a57570996b4f9d35adb161edf0861489e64
AkisArou <akisarou90@gmail.com>
2024-12-09 03:43:25 +0200

    disable mini.pairs

--- nvim/lua/plugins/mini-pairs.lua
+++ nvim/lua/plugins/mini-pairs.lua
@@ -1,11 +1,27 @@
+-- return {
+--   "windwp/nvim-autopairs",
+--   event = "InsertEnter",
+--   config = true,
+--   -- use opts = {} for passing setup options
+--   -- this is equivalent to setup({}) function
+-- }
+
 return {
   "echasnovski/mini.pairs",
+  enabled = false,
   version = false,
   event = "VeryLazy",
   opts = {
     mappings = {
-      ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%a\\].", register = { cr = false } },
-      ["<"] = { action = "open", pair = "<>", neigh_pattern = ".[%(]", register = { cr = false } },
+      -- ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%a\\].", register = { cr = false } },
+      -- ["<"] = { action = "open", pair = "<>", neigh_pattern = ".[%(]", register = { cr = false } },
+
+      ['"'] = {
+        action = "closeopen",
+        pair = '""',
+        neigh_pattern = " ", -- Only allowed when no neighbors or only spaces.
+        register = { cr = false },
+      },
     },
   },
 }
