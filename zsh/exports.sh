0be82713c08682e870eb15f99d81bbe650f9b66c
AkisArou <akisarou90@gmail.com>
2024-12-13 04:23:18 +0200

    nvim-lint

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -94,5 +94,7 @@ export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
 export NODE_OPTIONS="--max-old-space-size=8192 --disable-warning=ExperimentalWarning --experimental-transform-types"
 export NODE_COMPILE_CACHE="$HOME/.cache/node-compile-cache"
 
+export ESLINT_D_IDLE=1000000000
+
 export DESKTOP="192.168.1.2"
 export LAPTOP="192.168.1.3"
