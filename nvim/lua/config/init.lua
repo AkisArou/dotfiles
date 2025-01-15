c92e0be52e996b0e0cdec35ddd9481364f4d2552
AkisArou <akisarou90@gmail.com>
2025-01-15 07:11:43 +0200

    oversheer

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -5,14 +5,14 @@ require("config.keymaps")
 require("config.filetype")
 require("config.netrw")
 
-local cwd = vim.fn.getcwd()
+-- local cwd = vim.fn.getcwd()
 
-if cwd:match("nable%-solutions") then
-  local project = cwd .. "/tsconfig.json"
-
-  require("tsc.init").setup({
-    args = "--build " .. project .. " --watch",
-  })
-
-  vim.keymap.set("n", "<leader>cp", "<cmd>TSCOpen<cr>")
-end
+-- if cwd:match("nable%-solutions") then
+--   local project = cwd .. "/tsconfig.json"
+--
+--   require("tsc.init").setup({
+--     args = "--build " .. project .. " --watch",
+--   })
+--
+--   vim.keymap.set("n", "<leader>cp", "<cmd>TSCOpen<cr>")
+-- end
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -145,16 +145,18 @@ return {
       },
     })
 
-    ins_right({
-      function()
-        if TSC_ERRORS_COUNT == 0 or TSC_ERRORS_COUNT == nil then
-          return ""
-        end
+    ins_right("oversheer")
 
-        return "Project errors: " .. tostring(TSC_ERRORS_COUNT)
-      end,
-      color = { fg = colors.red, gui = "bold" },
-    })
+    -- ins_right({
+    --   function()
+    --     if TSC_ERRORS_COUNT == 0 or TSC_ERRORS_COUNT == nil then
+    --       return ""
+    --     end
+    --
+    --     return "Project errors: " .. tostring(TSC_ERRORS_COUNT)
+    --   end,
+    --   color = { fg = colors.red, gui = "bold" },
+    -- })
 
     ins_right({
       -- Lsp server name .
