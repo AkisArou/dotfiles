7e2de1ce27f369eb7cb1bd30b8c04c85ec8264fe
AkisArou <akisarou90@gmail.com>
2025-01-20 22:32:18 +0200

    lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -47,7 +47,12 @@ return {
           -- We are going to use lualine_c an lualine_x as left and
           -- right section. Both are highlighted by c theme .  So we
           -- are just setting default looks o statusline
-          normal = { c = { fg = colors.fg, bg = colors.bg } },
+          normal = {
+            c = {
+              fg = colors.fg,
+              -- bg = colors.bg
+            },
+          },
           inactive = { c = { fg = colors.fg, bg = colors.bg } },
         },
       },
