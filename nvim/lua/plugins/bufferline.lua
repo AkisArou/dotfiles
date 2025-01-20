389a73e2c7f404eacf8326b762180405c0c77d45
AkisArou <akisarou90@gmail.com>
2025-01-20 22:19:33 +0200

    bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,1 +1,14 @@
-return { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" }
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+
+  config = function()
+    vim.opt.termguicolors = true
+    require("bufferline").setup({
+      options = {
+        diagnostics = "nvim_lsp",
+      },
+    })
+  end,
+}
