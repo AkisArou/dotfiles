947d932248062bf3f11fcda035a963aa0ea95f5b
AkisArou <akisarou90@gmail.com>
2025-01-23 23:18:04 +0200

    rofi theme picker

--- rofi/theme.sh
+++ rofi/theme.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/zsh
 
-# Display a Rofi menu with the title "Theme" and two options: "vscode" and "tokyonight"
-theme=$(echo -e "vscode\ntokyonight" | ~/dotfiles/rofi/launch-rofi.sh -dmenu -p "Theme")
+theme=$(echo -e "vscode\ntokyonight\nonedark\ngruvbox" | ~/dotfiles/rofi/launch-rofi.sh -dmenu -p "Theme")
 
 ~/dotfiles/scripts/change-theme.sh $theme
