45d0a8d0729af79549898a162c5222bafb168658
AkisArou <akisarou90@gmail.com>
2025-02-05 03:15:23 +0200

    colorscheme onedark

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -28,36 +28,10 @@ local themes = {
     name = "onedark",
     repo = "navarasu/onedark.nvim",
     config = function()
-      local colors = require("onedark.palette").dark
-
-      -- black = "#181a1f",
-      -- bg0 = "#282c34",
-      -- bg1 = "#31353f",
-      -- bg2 = "#393f4a",
-      -- bg3 = "#3b3f4c",
-      -- bg_d = "#21252b",
-      -- bg_blue = "#73b8f1",
-      -- bg_yellow = "#ebd09c",
-      -- fg = "#abb2bf",
-      -- purple = "#c678dd",
-      -- green = "#98c379",
-      -- orange = "#d19a66",
-      -- blue = "#61afef",
-      -- yellow = "#e5c07b",
-      -- cyan = "#56b6c2",
-      -- red = "#e86671",
-      -- grey = "#5c6370",
-      -- light_grey = "#848b98",
-      -- dark_cyan = "#2b6f77",
-      -- dark_red = "#993939",
-      -- dark_yellow = "#93691d",
-      -- dark_purple = "#8a3fa0",
-      -- diff_add = "#31392b",
-      -- diff_delete = "#382b2c",
-      -- diff_change = "#1c3448",
-      -- diff_text = "#2c5372",
+      local colors = require("onedark.palette").darker
 
       require("onedark").setup({
+        style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
         highlights = {
           ["@keyword.import"] = { fg = colors.blue },
           ["@keyword"] = { fg = colors.purple },
