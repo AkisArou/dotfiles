cc4d82451af71c508a9d390bf0a5ea72869eb37d
AkisArou <akisarou90@gmail.com>
2025-02-06 04:38:24 +0200

    bigfile nvim

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,6 +3,11 @@ return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
+    bigfile = {
+      -- your bigfile configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
     ---@class snacks.lazygit.Config: snacks.terminal.Opts
     ---@field args? string[]
     ---@field theme? snacks.lazygit.Theme
