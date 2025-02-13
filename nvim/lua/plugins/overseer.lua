212d80ae412f525d3122ec3bc430bb290a0a2ddf
AkisArou <akisarou90@gmail.com>
2025-02-13 22:50:28 +0200

    overseer

--- nvim/lua/plugins/overseer.lua
+++ nvim/lua/plugins/overseer.lua
@@ -1,34 +1,6 @@
 return {
   "stevearc/overseer.nvim",
   config = function()
-    local problem_matcher = {
-      fileLocation = { "relative", "${cwd}" },
-      pattern = {
-        regexp = "^([^\\s].*)[\\(:](\\d+)[,:](\\d+)(?:\\):\\s+|\\s+-\\s+)(error|warning|info)\\s+TS(\\d+)\\s*:\\s*(.*)$",
-        vim_regexp = "\\v^([^[:space:]].*)[\\(:](\\d+)[,:](\\d+)%(\\):\\s+|\\s+-\\s+)(error|warning|info)\\s+TS(\\d+)\\s*:\\s*(.*)$",
-        file = 1,
-        line = 2,
-        column = 3,
-        severity = 4,
-        code = 5,
-        message = 6,
-      },
-      owner = "typescript",
-      source = "ts",
-      applyTo = "closedDocuments",
-      background = {
-        activeOnStart = true,
-        beginsPattern = {
-          regexp = "^\\s*(?:message TS6032:|\\[?\\D*.{1,2}[:.].{1,2}[:.].{1,2}\\D*(├\\D*\\d{1,2}\\D+┤)?(?:\\]| -)) (Starting compilation in watch mode|File change detected\\. Starting incremental compilation)\\.\\.\\.",
-          lua_pat = "(Starting compilation in watch mode|File change detected%. Starting incremental compilation)%.%.%.$",
-        },
-        endsPattern = {
-          regexp = "^\\s*(?:message TS6042:|\\[?\\D*.{1,2}[:.].{1,2}[:.].{1,2}\\D*(├\\D*\\d{1,2}\\D+┤)?(?:\\]| -)) (?:Compilation complete\\.|Found \\d+ errors?\\.) Watching for file changes\\.",
-          lua_pat = "Watching for file changes%.$",
-        },
-      },
-    }
-
     local overseer = require("overseer")
 
     overseer.setup({
@@ -39,35 +11,10 @@ return {
     })
 
     local cwd = vim.fn.getcwd()
-    local template_name = "nable-tsc"
-
     local is_nable = cwd:match("nable%-solutions")
 
-    overseer.register_template({
-      name = template_name,
-      condition = {
-        callback = function()
-          return is_nable
-        end,
-      },
-      builder = function()
-        return {
-          cmd = { "pnpm", "ts:watch" },
-          env = {
-            NO_COLOR = "1",
-          },
-          components = {
-            { "on_output_parse", problem_matcher = problem_matcher },
-            { "on_result_notify" },
-            { "on_result_diagnostics_quickfix" },
-            "default",
-          },
-        }
-      end,
-    })
-
     if is_nable then
-      overseer.run_template({ name = template_name, autostart = true })
+      overseer.run_template({ name = "tsc-project-references" })
     end
   end,
 }
