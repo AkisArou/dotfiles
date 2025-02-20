8433b22a22f04bdc8fe327aa10b338191f38c499
AkisArou <akisarou90@gmail.com>
2025-02-20 04:46:07 +0200

    nvim-eslint

--- nvim/lua/plugins/lsp/nvim-eslint.lua
+++ nvim/lua/plugins/lsp/nvim-eslint.lua
@@ -2,6 +2,18 @@ return {
   "esmuellert/nvim-eslint",
   config = function()
     require("nvim-eslint").setup({
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "javascript.jsx",
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+        "vue",
+        "svelte",
+        "astro",
+        "json",
+      },
       settings = {
         experimental = { useFlatConfig = true },
       },
