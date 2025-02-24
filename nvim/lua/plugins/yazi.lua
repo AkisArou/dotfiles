2c03e8db8276fdb31e4d5e6ceaa9e532d563d077
AkisArou <akisarou90@gmail.com>
2025-02-24 23:20:03 +0200

    yazi nvim

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -15,6 +15,7 @@ return {
   },
   config = function()
     require("yazi").setup({
+      open_for_directories = true,
       yazi_floating_window_border = "shadow",
       floating_window_scaling_factor = 1,
       future_features = {
--- yazi/init.lua
+++ yazi/init.lua
@@ -1,6 +1,5 @@
 require("git"):setup()
 
 require("session"):setup({
-	open_for_directories = true,
 	sync_yanked = true,
 })
