c025e60fbd84cce07568efa3b17ccb68a202041c
AkisArou <akisarou90@gmail.com>
2025-03-11 00:45:40 +0200

    eslint

--- nvim/lua/plugins/lsp/nvim-lint.lua
+++ nvim/lua/plugins/lsp/nvim-lint.lua
@@ -12,6 +12,7 @@ return {
       javascriptreact = { eslint },
       typescript = { eslint },
       typescriptreact = { eslint },
+      json = { eslint },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
