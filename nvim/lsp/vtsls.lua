926635b3505af9750b21b4ac87520b06c56c5d8b
AkisArou <akisarou90@gmail.com>
2025-04-16 14:52:51 +0300

    vtsls

--- nvim/lsp/vtsls.lua
+++ nvim/lsp/vtsls.lua
@@ -1,7 +1,7 @@
---- @type vim.lsp.Config
 local autoImportSpecifierExcludeRegex =
   "^(assert|async_hooks|buffer|child_process|cluster|console|crypto|dgram|dns|domain|events|fs|fs/promises|http|http2|https|inspector|module|net|os|path|path/posix|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|worker_threads|zlib)$"
 
+--- @type vim.lsp.Config
 return {
   root_dir = vim.fn.getcwd(),
   root_markers = { ".git", "tsconfig.json", "jsonconfig.json", "package.json" },
