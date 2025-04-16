c107c36094a45e679475946a1e0c3ba4f9b81d11
AkisArou <akisarou90@gmail.com>
2025-04-16 14:23:16 +0300

    nvim

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,20 +1,4 @@
 return {
   "mbbill/undotree",
   event = "VeryLazy",
-  config = function()
-    vim.cmd([[
-        if has("persistent_undo")
-          let target_path = expand('~/.undodir')
-
-            " create the directory and any parent directories
-            " if the location does not exist.
-            if !isdirectory(target_path)
-                call mkdir(target_path, "p", 0700)
-            endif
-
-            let &undodir=target_path
-            set undofile
-        endif
-    ]])
-  end,
 }
