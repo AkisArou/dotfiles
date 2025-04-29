161bd6f2f5419b0b64c38947109b0a4b0c948bfd
AkisArou <akisarou90@gmail.com>
2025-04-30 01:18:23 +0300

    nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,4 +2,13 @@ vim.g.is_work = vim.fn.getcwd():match("nable%-solutions") ~= nil
 vim.g.os_theme = os.getenv("THEME") or "onedark"
 
 require("config")
-require("custom")
+require("custom.pnpm")
+
+if vim.g.is_work then
+  local cwd = vim.fn.getcwd()
+  local project = cwd .. "/tsconfig.json"
+
+  require("custom.tsc").setup({
+    args = "--build " .. project .. " --watch",
+  })
+end
--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -6,12 +6,3 @@ require("config.lazy")
 require("config.lsp")
 require("config.diagnostic")
 require("config.netrw")
-
-if vim.g.is_work then
-  local cwd = vim.fn.getcwd()
-  local project = cwd .. "/tsconfig.json"
-
-  require("custom.tsc.init").setup({
-    args = "--build " .. project .. " --watch",
-  })
-end
