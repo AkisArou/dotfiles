84d704ee80f22bd75c080ede57ba3bd9a765210d
AkisArou <akisarou90@gmail.com>
2025-05-03 17:02:23 +0300

    neotest

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -18,22 +18,7 @@ return {
       },
     },
   },
-    -- stylua: ignore
-  keys = {
-    {"<leader>t", "", desc = "+test"},
-    { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File (Neotest)" },
-    ---@diagnostic disable-next-line: undefined-field
-    { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files (Neotest)" },
-    { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest (Neotest)" },
-    { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last (Neotest)" },
-    { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary (Neotest)" },
-    { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output (Neotest)" },
-    { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel (Neotest)" },
-    { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop (Neotest)" },
-    { "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end, desc = "Toggle Watch (Neotest)" },
-  },
   config = function()
-    local neotest_ns = vim.api.nvim_create_namespace("neotest")
     vim.diagnostic.config({
       virtual_text = {
         format = function(diagnostic)
@@ -42,7 +27,28 @@ return {
           return message
         end,
       },
-    }, neotest_ns)
+    }, vim.api.nvim_create_namespace("neotest"))
+
+    -- stylua: ignore
+    vim.keymap.set("n", "<leader>t", "", {desc = "+test"})
+    -- stylua: ignore
+    vim.keymap.set("n",  "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, {desc = "Run File (Neotest)" })
+    -- stylua: ignore
+    vim.keymap.set("n",  "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, {desc = "Run All Test Files (Neotest)" })
+    -- stylua: ignore
+    vim.keymap.set("n",  "<leader>tr", function() require("neotest").run.run() end, {desc = "Run Nearest (Neotest)" })
+    -- stylua: ignore
+    vim.keymap.set("n",  "<leader>tl", function() require("neotest").run.run_last() end, {desc = "Run Last (Neotest)" })
+    -- stylua: ignore
+    vim.keymap.set("n",  "<leader>ts", function() require("neotest").summary.toggle() end, {desc = "Toggle Summary (Neotest)" })
+    -- stylua: ignore
+    vim.keymap.set("n",  "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, {desc = "Show Output (Neotest)" })
+    -- stylua: ignore
+    vim.keymap.set("n",  "<leader>tO", function() require("neotest").output_panel.toggle() end, {desc = "Toggle Output Panel (Neotest)" })
+    -- stylua: ignore
+    vim.keymap.set("n",  "<leader>tS", function() require("neotest").run.stop() end, {desc = "Stop (Neotest)" })
+    -- stylua: ignore
+    vim.keymap.set("n",  "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end, {desc = "Toggle Watch (Neotest)" })
 
     require("neotest").setup({
       status = { virtual_text = true },
