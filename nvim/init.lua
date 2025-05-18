cedc74acf07b9ce0fe493ea8be92a432ec85c722
AkisArou <akisarou90@gmail.com>
2025-05-18 15:44:05 +0300

    revive

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ vim.g.is_work = vim.fn.getcwd():match("nable%-solutions") ~= nil
 vim.g.os_theme = os.getenv("THEME") or "tokyonight"
 
 require("config")
-require("custom.revive").setup({ auto = true })
+require("custom.revive").setup({ auto = false })
 require("custom.pnpm").setup()
 
 if vim.g.is_work then
