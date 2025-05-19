ae77294dd61a0455182307571a548db8c1797ff0
AkisArou <akisarou90@gmail.com>
2025-05-19 16:49:23 +0300

    st

--- st/config.def.h
+++ st/config.def.h
@@ -565,4 +565,4 @@ static char ascii_printable[] =
 #define UNDERCURL_SPIKY 1
 #define UNDERCURL_CAPPED 2
 // Active style
-#define UNDERCURL_STYLE UNDERCURL_SPIKY
+#define UNDERCURL_STYLE UNDERCURL_CURLY
