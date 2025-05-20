ea391b5fa9b7df401fa2987393260bc323121bc0
AkisArou <akisarou90@gmail.com>
2025-05-20 04:04:43 +0300

    diagnostic nvim

--- nvim/lua/config/diagnostic.lua
+++ nvim/lua/config/diagnostic.lua
@@ -17,3 +17,21 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
+
+local diagnostic_goto = function(next, severity)
+  return function()
+    vim.diagnostic.jump({
+      count = next and 1 or -1,
+      severity = severity and vim.diagnostic.severity[severity] or nil,
+    })
+  end
+end
+
+local map = vim.keymap.set
+
+map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
