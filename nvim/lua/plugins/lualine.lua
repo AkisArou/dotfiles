8ac7e1c7045d7ac8af4879e2723a524a1eda78fe
AkisArou <akisarou90@gmail.com>
2025-05-28 23:50:00 +0300

    lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -12,47 +12,57 @@ return {
       return "Project errors: " .. tostring(total_errors)
     end
 
+    local filename = function()
+      local get_fg = function(hl)
+        return { fg = string.format("#%06x", vim.api.nvim_get_hl(0, { name = hl }).fg), gui = "bold" }
+      end
+
+      return {
+        "filename",
+        color = function()
+          local diagnostics =
+            vim.diagnostic.count(0, { severity = { vim.diagnostic.severity.ERROR, vim.diagnostic.severity.WARN } })
+
+          local errors = diagnostics[vim.diagnostic.severity.ERROR] or 0
+          local warnings = diagnostics[vim.diagnostic.severity.WARN] or 0
+
+          local color = {}
+
+          if errors > 0 then
+            color = get_fg("DiagnosticError")
+          elseif warnings > 0 then
+            color = get_fg("DiagnosticWarning")
+          end
+
+          return color
+        end,
+      }
+    end
+
     require("lualine").setup({
       options = {
-        icons_enabled = true,
         theme = vim.g.os_theme,
-        disabled_filetypes = {
-          "man",
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        always_show_tabline = true,
-        globalstatus = false,
-        refresh = {
-          statusline = 100,
-          tabline = 100,
-          winbar = 100,
-        },
+        disabled_filetypes = { "man" },
+        component_separators = { left = "", right = "" },
       },
       sections = {
         lualine_a = { "mode" },
-        lualine_b = {
-          "branch",
-          "diff",
-        },
+        lualine_b = {},
         lualine_c = {
-          "filename",
-          {
-            "diagnostics",
-            symbols = { error = " ", warn = " ", info = " " },
-            update_in_insert = true,
-          },
+          filename(),
+          { "diagnostics", symbols = { error = " ", warn = " ", info = " " }, update_in_insert = true },
         },
         lualine_x = {
           "lsp_progress",
           { tsc, icon = "", color = "DiagnosticError" },
           "nvim-dap-ui",
-          "encoding",
-          "fileformat",
-          "filetype",
+          "branch",
+          "diff",
+          "progress",
+          "location",
         },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
+        lualine_y = {},
+        lualine_z = {},
       },
       inactive_sections = {
         lualine_a = {},
