25646f9312e3c1ecdfb2a96c4634d477a09daf71
AkisArou <akisarou90@gmail.com>
2025-07-02 02:55:35 +0300

    neogit

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -13,7 +13,7 @@ return {
       integrations = {
         -- If enabled, use telescope for menu selection rather than vim.ui.select.
         -- Allows multi-select and some things that vim.ui.select doesn't.
-        telescopeed = false,
+        telescope = false,
         -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
         -- The diffview integration enables the diff popup.
         --
