0ffd63d526d1f6f88b03d535189d86134585cf74
AkisArou <akisarou90@gmail.com>
2025-07-20 09:31:30 +0300

    vtsls

--- nvim/lsp/vtsls.lua
+++ nvim/lsp/vtsls.lua
@@ -1,6 +1,15 @@
 local autoImportSpecifierExcludeRegex =
   "^(assert|async_hooks|buffer|child_process|cluster|console|crypto|dgram|dns|domain|events|fs|fs/promises|http|http2|https|inspector|module|net|os|path|path/posix|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|worker_threads|zlib)$"
 
+local preferences = {
+  includePackageJsonAutoImports = "on",
+  importModuleSpecifier = "non-relative",
+  importModuleSpecifierEnding = "js",
+  autoImportSpecifierExcludeRegexes = {
+    autoImportSpecifierExcludeRegex,
+  },
+}
+
 --- @type vim.lsp.Config
 return {
   root_dir = vim.fn.getcwd(),
@@ -19,23 +28,13 @@ return {
       format = {
         enable = false,
       },
-      preferences = {
-        includePackageJsonAutoImports = "on",
-        importModuleSpecifier = "non-relative",
-        autoImportSpecifierExcludeRegexes = {
-          autoImportSpecifierExcludeRegex,
-        },
-      },
+      preferences = preferences,
     },
     javascript = {
       format = {
         enable = false,
       },
-      preferences = {
-        autoImportSpecifierExcludeRegexes = {
-          autoImportSpecifierExcludeRegex,
-        },
-      },
+      preferences = preferences,
     },
   },
 }
