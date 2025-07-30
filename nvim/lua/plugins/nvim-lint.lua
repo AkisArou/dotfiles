fb678d99707f052767b090bf1d0c84db81b7563e
AkisArou <akisarou90@gmail.com>
2025-07-31 02:57:04 +0300

    nvim-lint oxlint

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -1,23 +1,42 @@
 local lint = require("lint")
 
-vim.env.ESLINT_D_PPID = vim.fn.getpid()
+local js_linter = (function()
+  -- oxlint
+  local oxlint_bin = vim.fn.fnamemodify("./node_modules/.bin/" .. "oxlint", ":p")
+  local oxlint_stat = vim.loop.fs_stat(oxlint_bin)
 
-local eslint = "eslint_d"
+  if oxlint_stat then
+    local oxlint = require("lint").linters.oxlint
+
+    oxlint.cmd = function()
+      return oxlint_bin
+    end
+
+    return { "oxlint" }
+  end
+
+  -- eslint_d
+  local eslint_stat = vim.loop.fs_stat(vim.fn.fnamemodify("./node_modules/.bin/" .. "eslint", ":p"))
+
+  if eslint_stat then
+    vim.env.ESLINT_D_PPID = vim.fn.getpid()
+
+    return { "eslint_d" }
+  end
+
+  return {}
+end)()
 
 lint.linters_by_ft = {
-  javascript = { eslint },
-  javascriptreact = { eslint },
-  typescript = { eslint },
-  typescriptreact = { eslint },
-  json = { eslint },
+  javascript = js_linter,
+  javascriptreact = js_linter,
+  typescript = js_linter,
+  typescriptreact = js_linter,
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
   group = vim.api.nvim_create_augroup("lint", { clear = true }),
   callback = function()
-    -- Somehow with delay it works better
-    vim.defer_fn(function()
-      lint.try_lint()
-    end, 50)
+    lint.try_lint()
   end,
 })
