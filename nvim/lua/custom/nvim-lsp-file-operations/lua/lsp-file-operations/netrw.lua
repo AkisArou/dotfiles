72fc841d8b44b13e049903d72601e390355024d3
AkisArou <akisarou90@gmail.com>
2025-08-05 22:11:46 +0300

    netrw

--- nvim/lua/custom/nvim-lsp-file-operations/lua/lsp-file-operations/netrw.lua
+++ nvim/lua/custom/nvim-lsp-file-operations/lua/lsp-file-operations/netrw.lua
@@ -2,8 +2,7 @@ local M = {}
 
 local log = require("custom.nvim-lsp-file-operations.lua.lsp-file-operations.log")
 
--- Helper to get the current file or directory in netrw
-local function get_netrw_path()
+local function get_netrw_file_path()
   local dir = vim.g.NetrwBrowserDir or vim.fn.expand("%:p:h")
   local line = vim.fn.getline(".")
   local fname = line:match("^%s*(.+)$")
@@ -13,8 +12,13 @@ local function get_netrw_path()
   return dir
 end
 
+local function get_netrw_dir_path()
+  local file_path = get_netrw_file_path()
+  return vim.fn.fnamemodify(file_path, ":h")
+end
+
 function M.rename()
-  local old_name = get_netrw_path()
+  local old_name = get_netrw_file_path()
   local new_name = vim.fn.input("Rename to: ", old_name)
   if new_name ~= "" and new_name ~= old_name then
     require("custom.nvim-lsp-file-operations.lua.lsp-file-operations.will-rename").callback({
@@ -37,7 +41,7 @@ function M.delete()
   local response = nil
 
   local marked_files = vim.fn["netrw#Expose"]("netrwmarkfilelist")
-  local files = marked_files ~= "n/a" and marked_files or { get_netrw_path() }
+  local files = marked_files ~= "n/a" and marked_files or { get_netrw_file_path() }
 
   local to_delete = {}
 
@@ -77,16 +81,24 @@ function M.delete()
 end
 
 function M.create()
-  local dir = get_netrw_path()
+  local dir = get_netrw_dir_path()
   local new_name = vim.fn.input("Create file: ", dir .. "/")
+
   if new_name ~= "" then
     require("custom.nvim-lsp-file-operations.lua.lsp-file-operations.will-create").callback({ fname = new_name })
 
-    vim.cmd('silent! call writefile([], "' .. new_name .. '")')
+    -- Create the file using Lua
+    local file, err = io.open(new_name, "w") -- "w" mode creates or truncates the file
+    if not file then
+      vim.notify("Error creating file: " .. err, vim.log.levels.ERROR)
+      return
+    end
+    file:close()
 
     require("custom.nvim-lsp-file-operations.lua.lsp-file-operations.did-create").callback({ fname = new_name })
 
-    log.debug("Netrw create: " .. new_name)
+    vim.cmd("edit")
+    log.debug("Netrw create (Lua): " .. new_name)
   end
 end
 
