29269f8770b3b3940b491138531a641cebf0dfb3
AkisArou <akisarou90@gmail.com>
2025-10-13 12:30:32 +0300

    nvim colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,71 +2,28 @@ local themes = {
   tokyonight = function()
     require("tokyonight").setup({
       style = "night",
+      on_highlights = function(highlights, colors)
+        highlights["@tag.tsx"] = { fg = colors.magenta }
+        highlights["@variable.parameter"] = { fg = colors.fg_dark }
+        highlights["@variable.builtin.javascript"] = { fg = colors.teal }
+        highlights["@variable.builtin.typescript"] = { fg = colors.teal }
+        highlights["@variable.builtin.tsx"] = { fg = colors.teal }
+        highlights["@lsp.typemod.variable.defaultLibrary.typescriptreact"] = { fg = colors.teal }
+        highlights["Search"] = { bg = "#5e428f" }
+        highlights["IncSearch"] = { bg = colors.magenta, fg = colors.bg_dark }
+        highlights["Title"] = { fg = "#c0caf5" }
+        highlights["DiagnosticSignError"] = { fg = "#4a1b22" }
+        highlights["DiagnosticSignWarn"] = { fg = "#7a3f22" }
+        highlights["DiagnosticSignInfo"] = { fg = "#2a4373" }
+        highlights["DiagnosticSignHint"] = { fg = "#364a23" }
+        highlights["DiagnosticUnderlineError"] = { fg = "#4a1b22" }
+        highlights["DiagnosticVirtualTextError"] = { fg = "#3b4050" }
+        highlights["CursorLineNr"] = { fg = "#737aa2" }
+        highlights["DiagnosticUnnecessary"] = { fg = "#6F7E99" }
+      end,
     })
 
-    local colors = require("tokyonight.colors.storm")
-    -- ({
-    --   bg = "#24283b",
-    --   bg_dark = "#1f2335",
-    --   bg_dark1 = "#1b1e2d",
-    --   bg_highlight = "#292e42",
-    --   blue = "#7aa2f7",
-    --   blue0 = "#3d59a1",
-    --   blue1 = "#2ac3de",
-    --   blue2 = "#0db9d7",
-    --   blue5 = "#89ddff",
-    --   blue6 = "#b4f9f8",
-    --   blue7 = "#394b70",
-    --   comment = "#565f89",
-    --   cyan = "#7dcfff",
-    --   dark3 = "#545c7e",
-    --   dark5 = "#737aa2",
-    --   fg = "#c0caf5",
-    --   fg_dark = "#a9b1d6",
-    --   fg_gutter = "#3b4261",
-    --   green = "#9ece6a",
-    --   green1 = "#73daca",
-    --   green2 = "#41a6b5",
-    --   magenta = "#bb9af7",
-    --   magenta2 = "#ff007c",
-    --   orange = "#ff9e64",
-    --   purple = "#9d7cd8",
-    --   red = "#f7768e",
-    --   red1 = "#db4b4b",
-    --   teal = "#1abc9c",
-    --   terminal_black = "#414868",
-    --   yellow = "#e0af68",
-    --   git = {
-    --     add = "#449dab",
-    --     change = "#6183bb",
-    --     delete = "#914c54",
-    --   },
-    -- })
-
-    vim.defer_fn(function()
-      vim.cmd(string.format("hi @tag.tsx guifg=%s", colors.magenta))
-      vim.cmd(string.format("hi @variable.parameter guifg=%s", colors.fg_dark))
-      vim.cmd(string.format("hi @variable.parameter guifg=%s", colors.fg_dark))
-      vim.cmd(string.format("hi @variable.builtin.javascript guifg=%s", colors.teal))
-      vim.cmd(string.format("hi @variable.builtin.typescript guifg=%s", colors.teal))
-      vim.cmd(string.format("hi @variable.builtin.tsx guifg=%s", colors.teal))
-      vim.cmd(string.format("hi @lsp.typemod.variable.defaultLibrary.typescriptreact guifg=%s", colors.teal))
-      vim.cmd(string.format("hi Search guibg=%s", "#5e428f"))
-      vim.cmd(string.format("hi IncSearch guibg=%s guifg=%s", colors.magenta, colors.bg_dark))
-      vim.cmd("hi HighlightUrl guifg=#5f8ae9 gui=underline")
-
-      vim.cmd([[
-          hi Title guifg=#c0caf5
-          hi DiagnosticSignError guifg=#4a1b22 ctermfg=red
-          hi DiagnosticSignWarn guifg=#7a3f22 ctermfg=yellow
-          hi DiagnosticSignInfo guifg=#2a4373 ctermfg=blue
-          hi DiagnosticSignHint guifg=#364a23 ctermfg=green
-          hi DiagnosticUnderlineError guisp=#4a1b22 gui=undercurl
-          hi DiagnosticVirtualTextError guifg=#3b4050
-          hi CursorLineNr guifg=#737aa2
-          hi DiagnosticUnnecessary guifg=#6F7E99
-        ]])
-    end, 0)
+    vim.cmd("hi HighlightUrl guifg=#5f8ae9 gui=underline")
   end,
   vscode = function()
     vim.o.background = "dark"
