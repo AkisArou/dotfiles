0c86bcc959f9f8f4c448707ce8e4d2515bb04b07
AkisArou <akisarou90@gmail.com>
2025-10-30 12:33:32 +0200

    nvim is_difftool

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,7 @@
 vim.g.os_theme = os.getenv("THEME") or "tokyonight"
 
+vim.g.is_difftool = vim.iter(vim.v.argv):any(function(a)
+  return a:find("DiffTool") or a:find("difftool")
+end)
+
 require("config")
