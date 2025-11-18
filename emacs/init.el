5e53fc9d8125ece2ddafb69506b49347a8d216d0
AkisArou <akisarou90@gmail.com>
2025-11-18 16:06:03 +0200

    emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1211,10 +1211,6 @@
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (nerd-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
