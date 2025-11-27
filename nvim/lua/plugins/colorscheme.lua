55f1f6f14b5b75e095f16e9b9f8140d4d6e294e5
AkisArou <akisarou90@gmail.com>
2025-11-27 04:58:56 +0200

    colorscheme nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -20,7 +20,7 @@ local themes = {
         highlights["CursorLineNr"] = { fg = "#737aa2" }
         highlights["DiagnosticUnnecessary"] = { fg = "#6F7E99" }
         highlights["FloatBorder"] = { fg = "#737aa2" }
-        highlights["MatchParen"] = { fg = "#cc0063", bold = true }
+        highlights["MatchParen"] = { fg = "#9d7cd8", bold = true }
       end,
     })
 
@@ -81,42 +81,3 @@ local selectedTheme = vim.g.os_theme or "tokyonight"
 themes[selectedTheme]()
 
 pcall(vim.cmd.colorscheme, selectedTheme)
----@class Palette
-local ret = {
-  bg = "#24283b",
-  bg_dark = "#1f2335",
-  bg_dark1 = "#1b1e2d",
-  bg_highlight = "#292e42",
-  blue = "#7aa2f7",
-  blue0 = "#3d59a1",
-  blue1 = "#2ac3de",
-  blue2 = "#0db9d7",
-  blue5 = "#89ddff",
-  blue6 = "#b4f9f8",
-  blue7 = "#394b70",
-  comment = "#565f89",
-  cyan = "#7dcfff",
-  dark3 = "#545c7e",
-  dark5 = "#737aa2",
-  fg = "#c0caf5",
-  fg_dark = "#a9b1d6",
-  fg_gutter = "#3b4261",
-  green = "#9ece6a",
-  green1 = "#73daca",
-  green2 = "#41a6b5",
-  magenta = "#bb9af7",
-  magenta2 = "#ff007c",
-  orange = "#ff9e64",
-  purple = "#9d7cd8",
-  red = "#f7768e",
-  red1 = "#db4b4b",
-  teal = "#1abc9c",
-  terminal_black = "#414868",
-  yellow = "#e0af68",
-  git = {
-    add = "#449dab",
-    change = "#6183bb",
-    delete = "#914c54",
-  },
-}
-return ret
