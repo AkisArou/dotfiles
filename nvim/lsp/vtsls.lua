e79fec89b734d2fa7d0efaa21278bcb32edaca22
AkisArou <akisarou90@gmail.com>
2025-11-28 14:55:11 +0200

    vtsls

--- nvim/lsp/vtsls.lua
+++ nvim/lsp/vtsls.lua
@@ -1,12 +1,9 @@
-local autoImportSpecifierExcludeRegex =
-  "^(assert|async_hooks|buffer|child_process|cluster|console|crypto|dgram|dns|domain|events|fs|fs/promises|http|http2|https|inspector|module|net|os|path|path/posix|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|worker_threads|zlib)$"
-
 local preferences = {
   includePackageJsonAutoImports = "on",
   importModuleSpecifier = "non-relative",
   importModuleSpecifierEnding = "js",
   autoImportSpecifierExcludeRegexes = {
-    autoImportSpecifierExcludeRegex,
+    "^(assert|async_hooks|buffer|child_process|cluster|console|crypto|dgram|dns|domain|events|fs|fs/promises|http|http2|https|inspector|module|net|os|path|path/posix|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|worker_threads|zlib)$",
   },
 }
 
