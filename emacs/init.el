316ab92e47609657db5c9794f0109d2021947b8c
AkisArou <akisarou90@gmail.com>
2025-12-09 23:12:51 +0200

    emacs cape

--- emacs/init.el
+++ emacs/init.el
@@ -619,6 +619,21 @@
 		(project-file (styles fussy))
 		(lsp-capf (styles fussy basic))))
 
+;; Add extensions
+(use-package cape
+  :ensure t
+  :defer t
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-file)
+  ;; (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+  )
+
+
 ;;; NERD-ICONS-CORFU
 ;; Provides Nerd Icons to be used with CORFU.
 (use-package nerd-icons-corfu
