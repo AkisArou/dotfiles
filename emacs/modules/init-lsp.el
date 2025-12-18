1a4988e6fde1085112673cea8730787e7f833201
AkisArou <akisarou90@gmail.com>
2025-12-18 10:31:36 +0200

    emacs

--- emacs/modules/init-lsp.el
+++ emacs/modules/init-lsp.el
@@ -12,27 +12,27 @@
   :config
   ;; List of packages to ensure
   (defvar my-mason-packages
-	'("vtsls"
-	  "lua-language-server"
-	  "json-lsp"
-	  "html-lsp"
-	  "css-lsp"
-	  "gopls"
-	  "tailwindcss-language-server"
-	  "cssmodules-language-server"
-	  "docker-language-server"
-	  "docker-compose-language-service"
-	  "yaml-language-server"
-	  "clangd"
-	  "mdx_analyzer"
-	  "prettierd"
-	  "taplo"))
+    '("vtsls"
+      "lua-language-server"
+      "json-lsp"
+      "html-lsp"
+      "css-lsp"
+      "gopls"
+      "tailwindcss-language-server"
+      "cssmodules-language-server"
+      "docker-language-server"
+      "docker-compose-language-service"
+      "yaml-language-server"
+      "clangd"
+      "mdx_analyzer"
+      "prettierd"
+      "taplo"))
 
   ;; Initialize Mason and install packages
   (mason-ensure
    (lambda ()
-	 (dolist (pkg my-mason-packages)
-	   (ignore-errors (mason-install pkg))))))
+     (dolist (pkg my-mason-packages)
+       (ignore-errors (mason-install pkg))))))
 
 
 ;;; LSP
@@ -54,20 +54,20 @@
   :straight t
   :defer t
   :hook (;; Replace XXX-mode with concrete major mode (e.g. python-mode)
-		 (lsp-mode . lsp-enable-which-key-integration)  ;; Integrate with Which Key
-		 ((js-mode                                      ;; Enable LSP for JavaScript
-		   tsx-ts-mode                                  ;; Enable LSP for TSX
-		   typescript-ts-base-mode                      ;; Enable LSP for TypeScript
-		   css-mode                                     ;; Enable LSP for CSS
-		   js-ts-mode                                   ;; Enable LSP for JavaScript (TS mode)
-		   go-ts-mode                                   ;; Enable LSP for Go
-		   json-ts-mode                                 ;; Enable LSP for JSON
-		   web-mode) . lsp-deferred))                   ;; Enable LSP for Web (HTML)
+         (lsp-mode . lsp-enable-which-key-integration)  ;; Integrate with Which Key
+         ((js-mode                                      ;; Enable LSP for JavaScript
+           tsx-ts-mode                                  ;; Enable LSP for TSX
+           typescript-ts-base-mode                      ;; Enable LSP for TypeScript
+           css-mode                                     ;; Enable LSP for CSS
+           js-ts-mode                                   ;; Enable LSP for JavaScript (TS mode)
+           go-ts-mode                                   ;; Enable LSP for Go
+           json-ts-mode                                 ;; Enable LSP for JSON
+           html-mode) . lsp-deferred))                   ;; Enable LSP for Web (HTML)
   :commands lsp
   :config
   (set-face-attribute 'lsp-face-highlight-textual nil
-					  :background "#292e42"
-					  :weight 'normal)
+                      :background "#292e42"
+                      :weight 'normal)
   :custom
   (lsp-keymap-prefix "C-c l")                           ;; Set the prefix for LSP commands.
   (lsp-inlay-hint-enable nil)                           ;; Usage of inlay hints.
@@ -107,34 +107,34 @@
   (lsp-semantic-tokens-enable nil)                     ;; Disable semantic tokens.
   :preface
   (defun lsp-booster--advice-json-parse (old-fn &rest args)
-	"Try to parse bytecode instead of json."
-	(or
-	 (when (equal (following-char) ?#)
-	   (let ((bytecode (read (current-buffer))))
-		 (when (byte-code-function-p bytecode)
-		   (funcall bytecode))))
-	 (apply old-fn args)))
+    "Try to parse bytecode instead of json."
+    (or
+     (when (equal (following-char) ?#)
+       (let ((bytecode (read (current-buffer))))
+         (when (byte-code-function-p bytecode)
+           (funcall bytecode))))
+     (apply old-fn args)))
   (advice-add (if (progn (require 'json)
-						 (fboundp 'json-parse-buffer))
-				  'json-parse-buffer
-				'json-read)
-			  :around
-			  #'lsp-booster--advice-json-parse)
+                         (fboundp 'json-parse-buffer))
+                  'json-parse-buffer
+                'json-read)
+              :around
+              #'lsp-booster--advice-json-parse)
 
   (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
-	"Prepend emacs-lsp-booster command to lsp CMD."
-	(let ((orig-result (funcall old-fn cmd test?)))
-	  (if (and (not test?)                             ;; for check lsp-server-present?
-			   (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
-			   lsp-use-plists
-			   (not (functionp 'json-rpc-connection))  ;; native json-rpc
-			   (executable-find "emacs-lsp-booster"))
-		  (progn
-			(when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
-			  (setcar orig-result command-from-exec-path))
-			(message "Using emacs-lsp-booster for %s!" orig-result)
-			(cons "emacs-lsp-booster" orig-result))
-		orig-result)))
+    "Prepend emacs-lsp-booster command to lsp CMD."
+    (let ((orig-result (funcall old-fn cmd test?)))
+      (if (and (not test?)                             ;; for check lsp-server-present?
+               (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+               lsp-use-plists
+               (not (functionp 'json-rpc-connection))  ;; native json-rpc
+               (executable-find "emacs-lsp-booster"))
+          (progn
+            (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+              (setcar orig-result command-from-exec-path))
+            (message "Using emacs-lsp-booster for %s!" orig-result)
+            (cons "emacs-lsp-booster" orig-result))
+        orig-result)))
 
   :init
   (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
@@ -147,9 +147,9 @@
 ;;; LSP-VTSLS
 (use-package lsp-vtsls
   :straight (lsp-vtsls
-			 :type git
-			 :host github
-			 :repo "sdvcrx/lsp-vtsls")
+             :type git
+             :host github
+             :repo "sdvcrx/lsp-vtsls")
   :after lsp-mode
   :config
   ;; core vtsls settings
@@ -162,19 +162,19 @@
   ;; formatting disabled
   (lsp-register-custom-settings
    '(("typescript.format.enable" nil t)
-	 ("javascript.format.enable" nil t)))
+     ("javascript.format.enable" nil t)))
 
   ;; TS/JS preferences
   (let ((preferences
-		 (list :includePackageJsonAutoImports "on"
-			   :importModuleSpecifier "non-relative"
-			   :importModuleSpecifierEnding "js"
-			   :useAliasesForRenames nil
-			   :autoImportSpecifierExcludeRegexes
-			   (vector "^(assert|async_hooks|buffer|child_process|cluster|console|crypto|dgram|dns|domain|events|fs|fs/promises|http|http2|https|inspector|module|net|os|path|path/posix|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|worker_threads|zlib)$"))))
-	(lsp-register-custom-settings
-	 `(("typescript.preferences" ,preferences t)
-	   ("javascript.preferences" ,preferences t)))))
+         (list :includePackageJsonAutoImports "on"
+               :importModuleSpecifier "non-relative"
+               :importModuleSpecifierEnding "js"
+               :useAliasesForRenames nil
+               :autoImportSpecifierExcludeRegexes
+               (vector "^(assert|async_hooks|buffer|child_process|cluster|console|crypto|dgram|dns|domain|events|fs|fs/promises|http|http2|https|inspector|module|net|os|path|path/posix|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|worker_threads|zlib)$"))))
+    (lsp-register-custom-settings
+     `(("typescript.preferences" ,preferences t)
+       ("javascript.preferences" ,preferences t)))))
 
 ;; TODO: move
 (add-hook 'dired-mode-hook #'lsp-dired-mode)
@@ -199,52 +199,52 @@
 
   ;; Lint rules
   (setq lsp-tailwindcss-lint-css-conflict "error"
-		lsp-tailwindcss-lint-invalid-apply "error"
-		lsp-tailwindcss-lint-invalid-screen "error"
-		lsp-tailwindcss-lint-invalid-variant "error"
-		lsp-tailwindcss-lint-invalid-config-path "error"
-		lsp-tailwindcss-lint-invalid-tailwind-directive "error"
-		lsp-tailwindcss-lint-recommended-variant-order "warning")
+        lsp-tailwindcss-lint-invalid-apply "error"
+        lsp-tailwindcss-lint-invalid-screen "error"
+        lsp-tailwindcss-lint-invalid-variant "error"
+        lsp-tailwindcss-lint-invalid-config-path "error"
+        lsp-tailwindcss-lint-invalid-tailwind-directive "error"
+        lsp-tailwindcss-lint-recommended-variant-order "warning")
 
   ;; ;; classAttributes
   (setq lsp-tailwindcss-class-attributes
-		["class" "className" "style" "classList"])
+        ["class" "className" "style" "classList"])
 
   ;; ;; classFunctions
   (setq lsp-tailwindcss-class-functions
-		["cn" "clsx" "tw" "tw.color" "tw.style"]))
+        ["cn" "clsx" "tw" "tw.color" "tw.style"]))
 
 
 ;;; oxlint / oxc_language_server
 (defun my/oxlint-server-cmd ()
   "Return the path to the oxlint language server for the current project."
   (let ((server-path (expand-file-name "node_modules/.bin/oxc_language_server"
-									   (lsp-workspace-root))))
-	(if (file-executable-p server-path)
-		server-path
-	  (error "Oxc_language_server not found in node_modules/.bin"))))
+                                       (lsp-workspace-root))))
+    (if (file-executable-p server-path)
+        server-path
+      (error "Oxc_language_server not found in node_modules/.bin"))))
 
 (with-eval-after-load 'lsp-mode
   (lsp-register-client
    (make-lsp-client
-	:new-connection (lsp-stdio-connection #'my/oxlint-server-cmd)
-	:activation-fn (lsp-activate-on
-					"javascript"
-					"javascriptreact"
-					"typescript"
-					"typescriptreact")
-	:server-id 'oxlint
-	:add-on? t
-	:priority -1)))
+    :new-connection (lsp-stdio-connection #'my/oxlint-server-cmd)
+    :activation-fn (lsp-activate-on
+                    "javascript"
+                    "javascriptreact"
+                    "typescript"
+                    "typescriptreact")
+    :server-id 'oxlint
+    :add-on? t
+    :priority -1)))
 
 (defun my/oxlint-fix-all ()
   "Apply fixes to the current buffer using oxlint."
   (interactive)
   (when-let ((client (lsp--find-clients 'oxlint)))
-	(lsp-request
-	 "workspace/executeCommand"
-	 `(:command "oxc.fixAll"
-				:arguments [(:uri ,(lsp--buffer-uri))]))))
+    (lsp-request
+     "workspace/executeCommand"
+     `(:command "oxc.fixAll"
+                :arguments [(:uri ,(lsp--buffer-uri))]))))
 
 
 
