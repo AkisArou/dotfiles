79633c2d0bdfb5c8e1d06269e244deb996f8723d
AkisArou <akisarou90@gmail.com>
2026-01-07 13:55:33 +0200

    colorscheme nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -25,16 +25,16 @@ local themes = {
     })
 
     vim.cmd([[
-      hi FzfLuaHeaderText guifg=#434a65
-      hi FzfLuaHeaderBind guifg=#434a65
       hi manOptionDesc guifg=#41a6b5
     ]])
 
     vim.defer_fn(function()
       vim.cmd([[ 
-      hi HighlightUrl guifg=#5f8ae9 gui=underline
-      hi DiagnosticUnderlineError guisp=#7a3844 gui=undercurl
-    ]])
+        hi HighlightUrl guifg=#5f8ae9 gui=underline
+        hi DiagnosticUnderlineError guisp=#7a3844 gui=undercurl
+        hi FzfLuaHeaderText guifg=#434a65
+        hi FzfLuaHeaderBind guifg=#434a65
+      ]])
     end, 0)
   end,
   vscode = function()
