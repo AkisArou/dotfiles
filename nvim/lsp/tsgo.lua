9130a654b523667b9d2f47f6b9a8ce50eec92659
AkisArou <akisarou90@gmail.com>
2026-02-13 23:36:56 +0200

    tsgo

--- nvim/lsp/tsgo.lua
+++ nvim/lsp/tsgo.lua
@@ -1,22 +1,44 @@
-return {}
+local preferences = {
+  includePackageJsonAutoImports = "on",
+  importModuleSpecifier = "non-relative",
+  importModuleSpecifierEnding = "js",
+  useAliasesForRenames = false,
+  autoImportSpecifierExcludeRegexes = {
+    "^(assert|async_hooks|buffer|child_process|cluster|console|crypto|dgram|dns|domain|events|fs|fs/promises|http|http2|https|inspector|module|net|os|path|path/posix|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|worker_threads|zlib)$",
+  },
+}
 
-------@type vim.lsp.Config
----return {
----  cmd = function(dispatchers, config)
----    local cmd = "tsgo"
----    local local_cmd = (config or {}).root_dir and config.root_dir .. "/node_modules/.bin/tsgo"
----    if local_cmd and vim.fn.executable(local_cmd) == 1 then
----      cmd = local_cmd
----    end
----    return vim.lsp.rpc.start({ cmd, "--lsp", "--stdio" }, dispatchers)
----  end,
----  filetypes = {
----    "javascript",
----    "javascriptreact",
----    "javascript.jsx",
----    "typescript",
----    "typescriptreact",
----    "typescript.tsx",
----  },
----  root_dir = vim.fn.getcwd(),
----}
+---@type vim.lsp.Config
+return {
+  cmd = function(dispatchers, config)
+    local cmd = "tsgo"
+    local local_cmd = (config or {}).root_dir and config.root_dir .. "/node_modules/.bin/tsgo"
+    if local_cmd and vim.fn.executable(local_cmd) == 1 then
+      cmd = local_cmd
+    end
+    return vim.lsp.rpc.start({ cmd, "--lsp", "--stdio" }, dispatchers)
+  end,
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+  root_dir = vim.fn.getcwd(),
+  settings = {
+    typescript = {
+      format = {
+        enable = false,
+      },
+      preferences = preferences,
+    },
+    javascript = {
+      format = {
+        enable = false,
+      },
+      preferences = preferences,
+    },
+  },
+}
--- nvim/lsp/vtsls.lua
+++ nvim/lsp/vtsls.lua
@@ -1,47 +1,47 @@
-local preferences = {
-  includePackageJsonAutoImports = "on",
-  importModuleSpecifier = "non-relative",
-  importModuleSpecifierEnding = "js",
-  useAliasesForRenames = false,
-  autoImportSpecifierExcludeRegexes = {
-    "^(assert|async_hooks|buffer|child_process|cluster|console|crypto|dgram|dns|domain|events|fs|fs/promises|http|http2|https|inspector|module|net|os|path|path/posix|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|worker_threads|zlib)$",
-  },
-}
-
---- @type vim.lsp.Config
-return {
-  cmd = { "vtsls", "--stdio" },
-  root_dir = vim.fn.getcwd(),
-  root_markers = { ".git", "tsconfig.json", "jsonconfig.json", "package.json" },
-  filetypes = {
-    "javascript",
-    "javascriptreact",
-    "javascript.jsx",
-    "typescript",
-    "typescriptreact",
-    "typescript.tsx",
-  },
-  settings = {
-    vtsls = {
-      autoUseWorkspaceTsdk = true,
-      experimental = {
-        completion = {
-          enableServerSideFuzzyMatch = true,
-          entriesLimit = 5000,
-        },
-      },
-    },
-    typescript = {
-      format = {
-        enable = false,
-      },
-      preferences = preferences,
-    },
-    javascript = {
-      format = {
-        enable = false,
-      },
-      preferences = preferences,
-    },
-  },
-}
+-- local preferences = {
+--   includePackageJsonAutoImports = "on",
+--   importModuleSpecifier = "non-relative",
+--   importModuleSpecifierEnding = "js",
+--   useAliasesForRenames = false,
+--   autoImportSpecifierExcludeRegexes = {
+--     "^(assert|async_hooks|buffer|child_process|cluster|console|crypto|dgram|dns|domain|events|fs|fs/promises|http|http2|https|inspector|module|net|os|path|path/posix|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|worker_threads|zlib)$",
+--   },
+-- }
+--
+-- --- @type vim.lsp.Config
+-- return {
+--   cmd = { "vtsls", "--stdio" },
+--   root_dir = vim.fn.getcwd(),
+--   root_markers = { ".git", "tsconfig.json", "jsonconfig.json", "package.json" },
+--   filetypes = {
+--     "javascript",
+--     "javascriptreact",
+--     "javascript.jsx",
+--     "typescript",
+--     "typescriptreact",
+--     "typescript.tsx",
+--   },
+--   settings = {
+--     vtsls = {
+--       autoUseWorkspaceTsdk = true,
+--       experimental = {
+--         completion = {
+--           enableServerSideFuzzyMatch = true,
+--           entriesLimit = 5000,
+--         },
+--       },
+--     },
+--     typescript = {
+--       format = {
+--         enable = false,
+--       },
+--       preferences = preferences,
+--     },
+--     javascript = {
+--       format = {
+--         enable = false,
+--       },
+--       preferences = preferences,
+--     },
+--   },
+-- }
