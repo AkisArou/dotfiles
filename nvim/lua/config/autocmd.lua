98ff94cb5c973a00b5ece41c47d0f05812226ccf
AkisArou <akisarou90@gmail.com>
2026-02-16 16:10:32 +0200

    nvim autocmds

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -101,7 +101,7 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- wrap and check for spell in text filetypes
+-- check for spell in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup("wrap_spell"),
   pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
@@ -111,7 +111,6 @@ vim.api.nvim_create_autocmd("FileType", {
       return
     end
 
-    vim.opt_local.wrap = true
     vim.opt_local.spell = true
   end,
 })
