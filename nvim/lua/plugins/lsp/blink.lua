3b04ed1b2ac90a52fb41ecacb5d764a8e2bdc327
AkisArou <akisarou90@gmail.com>
2026-03-04 00:05:39 +0200

    blink emmet

--- nvim/lua/plugins/lsp/blink.lua
+++ nvim/lua/plugins/lsp/blink.lua
@@ -41,37 +41,37 @@ require("blink-cmp").setup({
     },
 
     providers = {
-      -- lsp = {
-      --   name = "LSP",
-      --   module = "blink.cmp.sources.lsp",
-      --   transform_items = function(ctx, items)
-      --     if vim.bo[ctx.bufnr].filetype ~= "typescriptreact" then
-      --       return items
-      --     end
-      --
-      --     return vim.tbl_filter(function(item)
-      --       if item.client_name ~= "emmet_language_server" then
-      --         return true
-      --       end
-      --
-      --       local node = vim.treesitter.get_node()
-      --       while node do
-      --         local t = node:type()
-      --         if
-      --           t == "jsx_element"
-      --           or t == "jsx_self_closing_element"
-      --           or t == "jsx_opening_element"
-      --           or t == "jsx_fragment"
-      --           or t == "jsx_expression"
-      --         then
-      --           return true
-      --         end
-      --         node = node:parent()
-      --       end
-      --       return false
-      --     end, items)
-      --   end,
-      -- },
+      lsp = {
+        name = "LSP",
+        module = "blink.cmp.sources.lsp",
+        transform_items = function(ctx, items)
+          if vim.bo[ctx.bufnr].filetype ~= "typescriptreact" then
+            return items
+          end
+
+          return vim.tbl_filter(function(item)
+            if item.client_name ~= "emmet_language_server" then
+              return true
+            end
+
+            local node = vim.treesitter.get_node()
+            while node do
+              local t = node:type()
+              if
+                t == "jsx_element"
+                or t == "jsx_self_closing_element"
+                or t == "jsx_opening_element"
+                or t == "jsx_fragment"
+                or t == "jsx_expression"
+              then
+                return true
+              end
+              node = node:parent()
+            end
+            return false
+          end, items)
+        end,
+      },
 
       lazydev = {
         name = "LazyDev",
