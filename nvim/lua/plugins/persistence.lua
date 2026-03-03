943d13e58d83112b078f5958d7dfa91dbee94020
AkisArou <akisarou90@gmail.com>
2026-03-03 03:24:39 +0200

    persistence nvim clear buffs

--- nvim/lua/plugins/persistence.lua
+++ nvim/lua/plugins/persistence.lua
@@ -3,3 +3,14 @@ require("persistence").setup()
 vim.keymap.set("n", "<leader>sl", function()
   require("persistence").load()
 end)
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "PersistenceSavePre",
+  callback = function()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.bo[buf].filetype == "opencode_output" or vim.bo[buf].filetype == "opencode" then
+        vim.api.nvim_buf_delete(buf, { force = true })
+      end
+    end
+  end,
+})
