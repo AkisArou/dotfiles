08d2331b0fa85590bca77b904b8d4db54e722dfc
AkisArou <akisarou90@gmail.com>
2026-03-05 22:56:55 +0200

    dwl alwayscenter

--- dwl/dwl.c
+++ dwl/dwl.c
@@ -2415,6 +2415,8 @@ setmon(Client *c, Monitor *m, uint32_t newtags)
 		/* Make sure window actually overlaps with the monitor */
 		resize(c, c->geom, 0);
 		c->tags = newtags ? newtags : m->tagset[m->seltags]; /* assign tags of target monitor */
+		c->prev.x = (m->w.width - c->prev.width) / 2 + m->m.x;
+		c->prev.y = (m->w.height - c->prev.height) / 2 + m->m.y;
 		setfullscreen(c, c->isfullscreen); /* This will call arrange(c->mon) */
 		setfloating(c, c->isfloating);
 	}
