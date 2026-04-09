a3a417be5a069d0bedb67cec33ee69d6de977302
AkisArou <akisarou90@gmail.com>
2026-04-09 20:54:51 +0300

    rm spiders

--- spider-wm/layouts/genymotion/index.tsx
+++
@@ -1,9 +0,0 @@
-import "./index.css"
-
-export default function layout() {
-  return (
-    <workspace id="root">
-      <slot id="main" />
-    </workspace>
-  );
-}
--- spiders-wm/layouts/genymotion/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function layout() {
-  return (
-    <workspace id="root">
-      <slot id="main" />
-    </workspace>
-  );
-}
