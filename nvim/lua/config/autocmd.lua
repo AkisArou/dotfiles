003d79eb06f121d90c5ab78c8f34fd814bdffd71
AkisArou <akisarou90@gmail.com>
2026-05-02 00:24:22 +0300

    nvim osc7

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -210,6 +210,27 @@ vim.api.nvim_create_autocmd("TermOpen", {
   end,
 })
 
+-- OSC 7
+vim.api.nvim_create_autocmd({ "TermRequest" }, {
+  group = augroup("terminal-osc7"),
+  desc = "Handles OSC 7 dir change requests",
+  callback = function(ev)
+    local val, n = string.gsub(ev.data.sequence, "\027]7;file://[^/]*", "")
+    if n > 0 then
+      -- OSC 7: dir-change
+      local dir = val
+      if vim.fn.isdirectory(dir) == 0 then
+        vim.notify("invalid dir: " .. dir)
+        return
+      end
+      vim.b[ev.buf].osc7_dir = dir
+      if vim.api.nvim_get_current_buf() == ev.buf then
+        vim.cmd.lcd(dir)
+      end
+    end
+  end,
+})
+
 -- Customize the built-in undotree window
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "nvim-undotree",
