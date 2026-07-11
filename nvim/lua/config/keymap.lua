b772123426be773ab203c80407efd7b6455ec034
AkisArou <akisarou90@gmail.com>
2026-07-11 23:27:40 +0300

    nvim_win_resize

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -78,7 +78,7 @@ map("n", "<leader>`", function()
   vim.cmd.vnew()
   vim.cmd.term()
   vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 15)
+  vim.api.nvim_win_resize(0, -1, 15, {})
 end)
 
 map("n", "<C-w><C-e>", function()
