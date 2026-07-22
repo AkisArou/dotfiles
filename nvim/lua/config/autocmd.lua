0ebe3dd72039533c7a4c65138054b618d3031703
AkisArou <akisarou90@gmail.com>
2026-07-22 11:38:40 +0300

    treesitter foldexpr

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -19,6 +19,8 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     pcall(vim.treesitter.start)
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    vim.wo.foldmethod = "expr"
   end,
 })
 
